70783a7722e57470f9cd003b8defb2121d5c7518
Daniel Morton <danielpmorton@gmail.com>
2023-01-02 13:46:45 -0800

    add pybullet hello world example

---
+++ src/examples/pyb_hello_world.py
@@ -0,0 +1,24 @@
+"""Hello World example from the Pybullet quickstart guide
+
+https://docs.google.com/document/d/10sXEhzFRSnvFcl3XxNGhnD4N2SedqwdAvK3dsihxVUA
+"""
+
+import time
+
+import pybullet as p
+import pybullet_data
+
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
+p.setGravity(0, 0, -10)
+planeId = p.loadURDF("plane.urdf")
+startPos = [0, 0, 1]
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+boxId = p.loadURDF("r2d2.urdf", startPos, startOrientation)
+# set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+for i in range(10000):
+    p.stepSimulation()
+    time.sleep(1.0 / 240.0)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos, cubeOrn)
+p.disconnect()
