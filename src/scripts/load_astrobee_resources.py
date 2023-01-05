d29950f949379d7ea2459efdea8cfb244b6b34a7
Daniel Morton <danielpmorton@gmail.com>
2023-01-04 16:54:05 -0800

    update loading script to include new URDFs

--- src/scripts/load_astrobee_resources.py
+++ src/scripts/load_astrobee_resources.py
@@ -1,4 +1,8 @@
 """Script to load Astrobee URDFs and view them in a Pybullet GUI window
+
+TODO
+- Make the ISS collision debugging OBJ file import as white rather than black
+- Get bullet to import the right colors for the astrobee? May need to specify in URDF
 """
 
 import time
@@ -15,11 +19,19 @@ def main():
     iss_urdf = "src/resources/iss.urdf"
     # Debugging URDF for collision visualization
     astrobee_debug_urdf = "src/resources/astrobee_collision.urdf"
+    iss_debug_urdf = "src/resources/iss_collision.urdf"
 
+    # Some starting positions for the astrobee within the ISS
+    cupola = (-5.0, 0.0, 5)  # Don't use this one for now until the mesh is fixed
+    us_lab = (3.0, 0.0, 5)
+
+    # Loading the standard ISS and Astrobee
     load_urdf(iss_urdf, fixed=True)
-    # load_urdf(astrobee_urdf, pos=(-5.0, 0.0, 5))
-    # load_urdf(astrobee_urdf)
-    load_urdf(astrobee_debug_urdf, pos=(-5.0, 0.0, 5))
+    load_urdf(astrobee_urdf, pos=us_lab)
+
+    # Loading the "debugging" versions to visualize the collision info
+    # load_urdf(iss_debug_urdf, fixed=True)
+    # load_urdf(astrobee_debug_urdf, pos=us_lab)
 
     time.sleep(10)
     for _ in range(100000):
