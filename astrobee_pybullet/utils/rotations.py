e68501ee5e9c2fe57ea295071b180944967045c6
dan <danielpmorton@gmail.com>
2023-02-06 21:39:20 -0800

    add transformation/rotation code

---
+++ astrobee_pybullet/utils/rotations.py
@@ -0,0 +1,367 @@
+"""Helper functions associated with rotations
+
+Notation for rotation matrices:
+A
+  R
+B
+describes the orientation of frame B with respect to frame A. AKA "B in A", or "B to A"
+In code, this can be described as R_B_in_A or R_B2A
+
+The columns of this matrix are the unit vectors of B expressed in the coordinates A
+The rows of this matrix are the unit vectors of A expressed in the coordinates of B
+
+Composing rotations:
+A       A     B     C
+  R  =    R     R     R
+D       B     C     D
+e.g. R_D2A = R_B2A @ R_C2B @ R_D2C
+
+All angles are in radians
+
+TODO
+- Make rmat_to_euler_angles() (More general version with multiple conventions)
+- Make rmat_to_fixed_angles() (More general version with multiple conventions)
+- Make sure naming conventions are consistent
+- Finish any NotImplemented functions
+"""
+
+import numpy as np
+import numpy.typing as npt
+
+
+def Rx(theta: float) -> np.ndarray:
+    """Rotation matrix for a rotation by theta radians about the X axis
+
+    As an operator: Rx @ P will rotate point P by theta about the X axis
+    As a mapping: Gives R_B_in_A ({A/B}R), where frame B is rotated theta radians about A's X axis
+    """
+    return np.array(
+        [
+            [1, 0, 0],
+            [0, np.cos(theta), -np.sin(theta)],
+            [0, np.sin(theta), np.cos(theta)],
+        ]
+    )
+
+
+def Ry(theta: float) -> np.ndarray:
+    """Rotation matrix for a rotation by theta radians about the Y axis
+
+    As an operator: Ry @ P will rotate point P by theta about the Y axis
+    As a mapping: Gives R_B_in_A ({A/B}R), where frame B is rotated theta radians about A's Y axis
+    """
+    return np.array(
+        [
+            [np.cos(theta), 0, np.sin(theta)],
+            [0, 1, 0],
+            [-np.sin(theta), 0, np.cos(theta)],
+        ]
+    )
+
+
+def Rz(theta: float) -> np.ndarray:
+    """Rotation matrix for a rotation by theta radians about the Z axis
+
+    As an operator: Rz @ P will rotate point P by theta about the Z axis
+    As a mapping: Gives R_B_in_A ({A/B}R), where frame B is rotated theta radians about A's Z axis
+    """
+    return np.array(
+        [
+            [np.cos(theta), -np.sin(theta), 0],
+            [np.sin(theta), np.cos(theta), 0],
+            [0, 0, 1],
+        ]
+    )
+
+
+def euler_zyx_to_rmat(th_z: float, th_y: float, th_x: float) -> np.ndarray:
+    """ZYX 3-angle Euler rotation matrix
+
+    Args:
+        th_z (float): Starting with frame A, angle to rotate about Z_A to obtain intermediate frame B'
+        th_y (float): Angle to rotate about Y_B' to obtain intermediate frame B''
+        th_x (float): Angle to rotate about X_B'' to obtain B
+
+    Returns:
+        np.ndarray: R_B_in_A ({A/B}R) where frame B is composed by three rotations in ZYX order starting from frame A
+    """
+    return Rz(th_z) @ Ry(th_y) @ Rx(th_x)
+
+
+def euler_xyz_to_rmat(th_x: float, th_y: float, th_z: float) -> np.ndarray:
+    """XYZ 3-angle Euler rotation matrix
+
+    Args:
+        th_x (float): Starting with frame A, angle to rotate about X_A to obtain intermediate frame B'
+        th_y (float): Angle to rotate about Y_B' to obtain intermediate frame B''
+        th_z (float): Angle to rotate about Z_B'' to obtain B
+
+    Returns:
+        np.ndarray: R_B_in_A ({A/B}R) where frame B is composed by three rotations in XYZ order starting from frame A
+    """
+    return Rx(th_x) @ Ry(th_y) @ Rz(th_z)
+
+
+def rmat_to_axis_angle(rmat: np.ndarray) -> tuple[np.ndarray, float]:
+    """Converts a rotation matrix into an axis-angle representation
+
+    Args:
+        rmat (np.ndarray): (3, 3) rotation matrix
+
+    Raises:
+        ZeroDivisionError: If the rotation matrix leads to a singularity
+            when represented in axis-angle form
+
+    Returns:
+        Tuple of:
+            np.ndarray: Axis of rotation. Shape (3,)
+            float: Rotation angle
+    """
+    angle = np.arccos((rmat[0, 0] + rmat[1, 1] + rmat[2, 2] - 1) / 2)
+    if np.abs(np.sin(angle)) < 1e-14:
+        raise ZeroDivisionError("Cannot convert to axis-angle: Near singularity")
+    axis = (1 / (2 * np.sin(angle))) * np.array(
+        [rmat[2, 1] - rmat[1, 2], rmat[0, 2] - rmat[2, 0], rmat[1, 0] - rmat[0, 1]]
+    )
+    return axis, angle
+
+
+def axis_angle_to_rmat(axis: list[float], angle: float) -> np.ndarray:
+    """Converts an axis-angle representation to a rotation matrix
+
+    Args:
+        axis (list[float]): Axis to rotate around: [x, y, z]
+        angle (float): Angle to rotate (radians)
+
+    Returns:
+        np.ndarray: Rotation matrix equivalent for the axis-angle representation
+    """
+    # Converting to a slightly more compact notation
+    kx, ky, kz = axis
+    ct = np.cos(angle)
+    st = np.sin(angle)
+    vt = 1 - ct
+    return np.array(
+        [
+            [kx * kx * vt + ct, kx * ky * vt - kz * st, kx * kz * vt + ky * st],
+            [kx * ky * vt + kz * st, ky * ky * vt + ct, ky * kz * vt - kx * st],
+            [kx * kz * vt - ky * st, ky * kz * vt + kx * st, kz * kz * vt + ct],
+        ]
+    )
+
+
+def euler_angles_to_rmat(convention: str, *angles: float) -> np.ndarray:
+    """Converts euler angles of a specified convention (like 'xyz') to a rotation matrix
+
+    Args:
+        convention (str): Rotation axis order for the specified angles: Must be some
+            permutation of 'xyz' - e.g. 'zyx', 'yxy', 'xyxyxyxyxy', ... and the length
+            of the string must match the number of angles in the inputs
+        *angles (float): Angle arguments. Length must match the convention string length
+
+    Returns:
+        np.ndarray: (3,3) rotation matrix
+    """
+    if not len(angles) == len(convention):
+        raise ValueError("Number of angles must match the specified convention")
+    if not all(c in {"x", "y", "z"} for c in convention.lower()):
+        raise ValueError("Convention must only include x, y, and z")
+    funcs = {"x": Rx, "y": Ry, "z": Rz}
+    R = np.eye(3)
+    for angle, axis in zip(angles, convention):
+        R = R @ funcs[axis](angle)
+    return R
+
+
+def fixed_angles_to_rmat(convention: str, *angles: float) -> np.ndarray:
+    """Converts fixed angles of a specified convention (like 'xyz') to a rotation matrix
+
+    Args:
+        convention (str): Rotation axis order for the specified angles: Must be some
+            permutation of 'xyz' - e.g. 'zyx', 'yxy', 'xyxyxyxyxy', ... and the length
+            of the string must match the number of angles in the inputs
+        *angles (float): Angle arguments. Length must match the convention string length
+
+    Returns:
+        np.ndarray: (3,3) rotation matrix
+    """
+    return euler_angles_to_rmat(convention[::-1], *angles[::-1])
+
+
+def rmat_to_euler_zyx(rmat: np.ndarray) -> tuple[float, float, float]:
+    """Converts a rotation matrix to Euler ZYX angles
+
+    Args:
+        rmat (np.ndarray): (3,3) rotation matrix
+
+    Raises:
+        ZeroDivisionError: If there is a singularity in the representation
+
+    Returns:
+        tuple[float, float, float]: ZYX Euler angles
+    """
+    [r11, _, _], [r21, _, _], [r31, r32, r33] = rmat
+    beta = np.arctan2(-r31, np.sqrt(r11**2 + r21**2))
+    cb = np.cos(beta)
+    if abs(cb) < 1e-14:
+        raise ZeroDivisionError("At a singularity")
+    alpha = np.arctan2(r21 / cb, r11 / cb)
+    gamma = np.arctan2(r32 / cb, r33 / cb)
+    return alpha, beta, gamma
+
+
+def rmat_to_euler_xyz(rmat: np.ndarray) -> tuple[float, float, float]:
+    """Converts a rotation matrix to Euler XYZ angles
+
+    Args:
+        rmat (np.ndarray): (3,3) rotation matrix
+
+    Raises:
+        ZeroDivisionError: If there is a singularity in the representation
+
+    Returns:
+        tuple[float, float, float]: XYZ Euler angles
+    """
+    [r11, r12, r13], [_, _, r23], [_, _, r33] = rmat
+    beta = np.arctan2(r13, np.sqrt(r23**2 + r33**2))
+    cb = np.cos(beta)
+    if abs(cb) < 1e-14:
+        raise ZeroDivisionError("At a singularity")
+    alpha = np.arctan2(-r23 / cb, r33 / cb)
+    gamma = np.arctan2(-r12 / cb, r11 / cb)
+    return alpha, beta, gamma
+
+
+def rmat_to_fixed_xyz(rmat: np.ndarray) -> tuple[float, float, float]:
+    """Converts a rotation matrix to Fixed XYZ angles
+
+    Args:
+        rmat (np.ndarray): (3,3) rotation matrix
+
+    Returns:
+        tuple[float, float, float]: XYZ Fixed angles
+    """
+    return rmat_to_euler_zyx(rmat)[::-1]
+
+
+def rmat_to_fixed_zyx(rmat: np.ndarray) -> tuple[float, float, float]:
+    """Converts a rotation matrix to Fixed ZYX angles
+
+    Args:
+        rmat (np.ndarray): (3,3) rotation matrix
+
+    Returns:
+        tuple[float, float, float]: ZYX Fixed angles
+    """
+    return rmat_to_euler_xyz(rmat)[::-1]
+
+
+def rmat_to_euler_params(rmat: np.ndarray, eta: int = 1) -> list[float]:
+    """Converts a rotation matrix into Euler parameters
+
+    Args:
+        rmat (np.ndarray): (3,3) rotation matrix
+        eta (int, optional): Parameter for sign determination, must be either 1 or -1. Defaults to 1.
+
+    Returns:
+        list[float]: The four Euler parameters
+    """
+    if eta not in {1, -1}:
+        raise ValueError(f"Invalid eta: {eta}. Must be 1 or -1")
+    [r11, r12, r13], [r21, r22, r23], [r31, r32, r33] = rmat
+    # Perform an initial solve for the parameters
+    l3 = (eta / 2) * np.sqrt(r11 + r22 + r33 + 1)
+    l2 = (eta / 2) * np.sign(r21 - r12) * np.sqrt(-r11 - r22 + r33 + 1)
+    l1 = (eta / 2) * np.sign(r13 - r31) * np.sqrt(-r11 + r22 - r33 + 1)
+    l0 = (eta / 2) * np.sign(r32 - r23) * np.sqrt(r11 - r22 - r33 + 1)
+    params = [l0, l1, l2, l3]
+    # Update the solution based on which is largest
+    d0 = 2 * np.sign(l0) * np.sqrt(r11 + r22 + r33 + 1)
+    d1 = 2 * np.sign(l1) * np.sqrt(r11 - r22 - r33 + 1)
+    d2 = 2 * np.sign(l2) * np.sqrt(-r11 + r22 - r33 + 1)
+    d3 = 2 * np.sign(l3) * np.sqrt(-r11 - r22 + r33 + 1)
+    max_ind = np.argmax(np.abs(params))
+    if max_ind == 0:
+        l0 = d0 / 4
+        l1 = (r32 - r23) / d0
+        l2 = (r13 - r31) / d0
+        l3 = (r21 - r12) / d0
+    elif max_ind == 1:
+        l0 = (r32 - r23) / d1
+        l1 = d1 / 4
+        l2 = (r21 + r12) / d1
+        l3 = (r13 + r31) / d1
+    elif max_ind == 2:
+        l0 = (r13 - r31) / d2
+        l1 = (r21 + r12) / d2
+        l2 = d2 / 4
+        l3 = (r32 + r23) / d2
+    else:  # max_ind == 3
+        l0 = (r21 - r12) / d3
+        l1 = (r13 + r31) / d3
+        l2 = (r32 + r23) / d3
+        l3 = d3 / 4
+    return [l0, l1, l2, l3]
+
+
+def euler_params_to_rmat(l0: float, l1: float, l2: float, l3: float) -> np.ndarray:
+    """Converts Euler parameters into a rotation matrix
+
+    Args:
+        l0 (float): 1st Euler parameter
+        l1 (float): 2nd Euler parameter
+        l2 (float): 3rd Euler parameter
+        l3 (float): 4th Euler parameter
+
+    Returns:
+        np.ndarray: Rotation matrix equivalent for the Euler parameter representation
+    """
+    return np.array(
+        [
+            [
+                2 * (l0**2 + l1**2) - 1,
+                2 * (l1 * l2 - l0 * l3),
+                2 * (l1 * l3 + l0 * l2),
+            ],
+            [
+                2 * (l1 * l2 + l0 * l3),
+                2 * (l0**2 + l2**2) - 1,
+                2 * (l2 * l3 - l0 * l1),
+            ],
+            [
+                2 * (l1 * l3 - l0 * l2),
+                2 * (l2 * l3 + l0 * l1),
+                2 * (l0**2 + l3**2) - 1,
+            ],
+        ]
+    )
+
+
+def check_rotation_mat(R: np.ndarray, atol: float = 1e-14) -> bool:
+    """Determines if a rotation matrix is valid by checking orthogonality and determinant
+
+    Args:
+        R (np.ndarray): A rotation matrix
+        atol (float): Absolute tolerance on the closeness checks. Defaults to 1e-14.
+
+    Returns:
+        bool: Whether or not R is a valid rotation matrix
+    """
+    if not np.shape(R) == (3, 3):
+        return False  # Incorrect size
+    if not np.allclose(R @ np.transpose(R), np.eye(R.shape[0]), atol=atol):
+        return False  # Its transpose is not its inverse
+    if not np.isclose(np.linalg.det(R), 1):
+        return False  # Determinant is not 1
+    return True
+
+
+def check_euler_params(l0: float, l1: float, l2: float, l3: float) -> bool:
+    # TODO!
+    raise NotImplementedError
+
+
+def rotate_point(rmat: np.ndarray, point: npt.ArrayLike):
+    # Use rotation matrix as operator within a single frame
+    # TODO
+    raise NotImplementedError
---
+++ astrobee_pybullet/utils/transformations.py
@@ -0,0 +1,102 @@
+"""Helper functions associated with transformations (rotations + translations)
+
+Notation for transformation matrices:
+A
+  T
+B
+describes the frame B with respect to frame A. AKA "B in A", or "B to A"
+In code, this can be described as T_B_in_A or T_B2A
+
+Composing transformations:
+A       A     B     C
+  T  =    T     T     T
+D       B     C     D
+e.g. T_D2A = T_B2A @ T_C2B @ T_D2C
+
+All angles are in radians
+"""
+
+from typing import Union
+
+import numpy as np
+
+from astrobee_pybullet.utils.rotations import check_rotation_mat
+
+
+def make_transform_mat(rot: np.ndarray, trans: np.ndarray) -> np.ndarray:
+    """Creates a transformation matrix from a rotation matrix and translation vector
+
+    TODO add checks that the rotation matrix is valid, and that the translation
+    is the correct size
+
+    Args:
+        rot (np.ndarray): (3,3) rotation matrix
+        trans (np.ndarray): (3,) translation vector
+
+    Returns:
+        np.ndarray: Transformation matrix, shape (4, 4)
+    """
+    T = np.eye(4, 4)
+    T[:3, :3] = rot
+    T[:3, 3] = trans
+    return T
+
+
+def check_transform_mat(T: np.ndarray) -> bool:
+    """Checks to see if a transformation matrix is valid
+
+    Args:
+        T (np.ndarray): (4, 4) transformation matrix
+
+    Returns:
+        bool: Whether or not the transformation matrix is valid
+    """
+    if not T.shape == (4, 4):
+        return False
+    rot = T[:3, :3]
+    last_row = T[3, :]
+    return check_rotation_mat(rot) and np.array_equal(
+        last_row, np.array([0.0, 0.0, 0.0, 1.0])
+    )
+
+
+def invert_transform_mat(T: np.ndarray) -> np.ndarray:
+    """Inverts a transformation matrix
+
+    Example: T_B2A = invert_transform_mat(T_A2B)
+
+    Args:
+        T (np.ndarray): (4, 4) transformation matrix
+
+    Returns:
+        np.ndarray: (4, 4) matrix representing the inverse transform of T
+    """
+    rot = T[:3, :3]
+    trans = T[:3, 3]
+    T_inv = np.eye(4)
+    T_inv[:3, :3] = np.transpose(rot)
+    T_inv[:3, 3] = -np.transpose(rot) @ trans
+    return T_inv
+
+
+def transform_point(
+    tmat: np.ndarray, point: Union[list[float], np.ndarray]
+) -> np.ndarray:
+    """Applies a transformation to a point
+
+    As a mapping: Changes the description of the point between frames
+        Example: point_in_B = transform_point(T_A2B, point_in_A)
+    As an operator: Moves a point within the same frame
+        Example: new_point = transform_point(transform, orig_point)
+
+    TODO: validate inputs?
+
+    Args:
+        tmat (np.ndarray): (4, 4) transformation matrix
+        point (Union[list[float], np.ndarray]): (3,) point to transform
+
+    Returns:
+        np.ndarray: (3,) transformed point
+    """
+    p = np.append(point, 1)  # Convert to (4,) array
+    return (tmat @ p)[:3]
