528b94fa107e164cc2a473e0dbed542bb1763f4d
dan <danielpmorton@gmail.com>
2023-02-19 22:40:43 -0800

    init plotting and trajectory utils

---
+++ pyastrobee/utils/plotting.py
@@ -0,0 +1,18 @@
+"""Functions for plotting various data or debugging info
+
+TODO look into pytransform3d.plot_utils
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pytransform3d.transformations import plot_transform
+from pytransform3d.plot_utils import make_3d_axis
+
+
+def plot_astrobee_frames():
+    raise NotImplementedError
+
+
+def plot_trajectory():
+    raise NotImplementedError
---
+++ pyastrobee/utils/trajectories.py
@@ -0,0 +1,15 @@
+"""Functions associated with trajectory calculation and generation
+
+TODO look into pytransform3d.trajectories
+"""
+
+# Need to figure out how sequences of pybullet constraints work
+# See if this matches up with how pytransform3d calculates things
+# Poses will be defined in world frame
+def create_robot_trajectory(start_pose, end_pose, duration):
+    pass
+
+
+# Does this function make any sense??
+def create_arm_trajectory():
+    pass
