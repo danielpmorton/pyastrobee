3bc68715bb9fcc2245d1689c504c3d439b0e9153
Daniel Morton <danielpmorton@gmail.com>
2023-03-01 14:47:14 -0800

    improve keyboard controller - add arm control, waypoint recording

--- pyastrobee/control/keyboard_controller.py
+++ pyastrobee/control/keyboard_controller.py
@@ -15,8 +15,15 @@ Usage:
     - k: - pitch
     - u: + yaw
     - o: - yaw
+- Arm control via arrow keys
+    - Up: + proximal angle
+    - Down: - proximal angle
+    - Right: + distal angle
+    - Left: - distal angle
 - Toggle opening/closing the gripper with G
-- Coarse control with capital letters
+- Record a waypoint with P
+- Coarse control of position/orientation with capital letters (via Shift)
+- Coarse control of the arm positioning via C
 - Press Space to switch reference frames (global <=> robot)
 - Press Esc to exit
 
@@ -27,10 +34,9 @@ NOTE
 - World frame control may not be very useful. Should we toggle between gripper frame and robot frame?
 
 TODO
-- Add raise/lower arm control
+- Unify the coarse control toggling between the position and the arm control
 - Multiprocessing!!! Change the hacky way I'm currently stepping through the simulation
 - Add force control, velocity control, and a toggle between methods
-- Change documentation back to capital letters for coarse control
 - Add back finer control of the gripper rather than just an open/close toggle?
 - Add back sshkeyboard?
 - Velocity / force control
@@ -38,7 +44,6 @@ TODO
 """
 
 import time
-import pprint
 
 import numpy as np
 import pybullet
@@ -65,6 +70,10 @@ class KeyboardController:
         self,
         robot: Astrobee,
     ):
+        # Disable keyboard shortcuts in pybullet so it doesn't do anything weird while we're controlling it
+        if pybullet.isConnected():
+            pybullet.configureDebugVisualizer(pybullet.COV_ENABLE_KEYBOARD_SHORTCUTS, 0)
+
         self.robot = robot
         self.gripper_is_open = robot.gripper_position >= 90
 
@@ -76,33 +85,27 @@ class KeyboardController:
         self.droll = 0.05  # rad
         self.dpitch = 0.05  # rad
         self.dyaw = 0.05  # rad
+        self.djoint = 0.05  # rad
 
         # Configuration for non-position/angular control keys
         self.frame_switch_key = keyboard.Key.space
         self.exit_key = keyboard.Key.esc
-        # Allow for either lowercase or capital G to control the gripper
-        self.gripper_keys = {
-            keyboard.KeyCode.from_char("g"),
-            keyboard.KeyCode.from_char("G"),
+        self.gripper_key = keyboard.KeyCode.from_char("g")
+        self.waypoint_key = keyboard.KeyCode.from_char("p")
+        self.coarse_control_key = keyboard.KeyCode.from_char("c")
+        self.arm_keys = {
+            keyboard.Key.up,
+            keyboard.Key.down,
+            keyboard.Key.right,
+            keyboard.Key.left,
         }
 
         # Coarse control multiplier
         self.mult = 5
 
-        # Current state of the frame being controlled
+        # States
         self.in_robot_frame = False
-
-        self.commands = {
-            "w/s": "+/- x",
-            "a/d": "+/- y",
-            "e/q": "+/- z",
-            "l/j": "+/- roll",
-            "i/k": "+/- pitch",
-            "u/o": "+/- yaw",
-            "Capital letters": "Coarse control",
-            "Space": "Switch reference frames",
-            "Esc": "Stop listening",
-        }
+        self.in_coarse_mode = False
 
         # Having this in position/Euler can be more interpretable than position/quaternion
         # Also, it is easier to scale Euler angles than quaternions for coarse control
@@ -131,10 +134,29 @@ class KeyboardController:
                 keyboard.KeyCode.from_char(key.upper())
             ] = pos_euler_xyz_to_pos_quat(self.mult * delta)
 
+    def _print_commands(self):
+        """Prints the keyboard/control action mapping information"""
+        print(
+            "Commands:\n"
+            + "w/s: +/- x\n"
+            + "a/d: +/- y\n"
+            + "e/q: +/- z\n"
+            + "l/j: +/- roll\n"
+            + "i/k: +/- pitch\n"
+            + "u/o: +/- yaw\n"
+            + "up/down: +/- arm proximal joint angle\n"
+            + "right/left: +/- arm distal joint angle\n"
+            + "c: Toggle coarse control for the arm\n"
+            + "g: Open/close the gripper\n"
+            + "p: Record a pose\n"
+            + "Space: Switch reference frames\n"
+            + "Esc: Stop listening"
+        )
+
     def start_listening(self):
         """Starts the keyboard listener"""
-        print("Now listening. Commands:")
-        pprint.pprint(self.commands)
+        print("Now listening.")
+        self._print_commands()
         with keyboard.Listener(
             on_press=self.on_press, on_release=self.on_release
         ) as listener:
@@ -182,9 +204,9 @@ class KeyboardController:
             # Toggle the reference frame and update our knowledge of the state
             self.in_robot_frame = not self.in_robot_frame
             print(
-                f"FRAME SWITCHED. Now in {'robot' if self.in_robot_frame else 'world'} frame"
+                f"\nFRAME SWITCHED. Now in {'robot' if self.in_robot_frame else 'world'} frame"
             )
-        elif key in self.gripper_keys:  # g or G
+        elif key == self.gripper_key:
             # Toggle the gripper and update our knowledge of the state
             if self.gripper_is_open:
                 self.robot.close_gripper()
@@ -192,7 +214,51 @@ class KeyboardController:
             else:
                 self.robot.open_gripper()
                 self.gripper_is_open = True
-            print(f"GRIPPER {'OPENED' if self.gripper_is_open else 'CLOSED'}")
+            print(f"\nGRIPPER {'OPENED' if self.gripper_is_open else 'CLOSED'}")
+        elif key == self.waypoint_key:
+            # Record a waypoint and print the info to the terminal
+            print(f"\nPose recorded: {self.robot.pose}")
+            print(f"Gripper position: {self.robot.gripper_position}")
+            print(f"Arm joints: {self.robot.arm_joint_angles}")
+        elif key == self.coarse_control_key:
+            # Toggle coarse control mode
+            self.in_coarse_mode = not self.in_coarse_mode
+            print(
+                f"\nCoarse control {'ACTIVATED' if self.in_coarse_mode else 'DEACTIVATED'}"
+            )
+        elif key in self.arm_keys:
+            cur_prox, cur_dist = self.robot.arm_joint_angles
+            prox_idx, dist_idx = self.robot.ARM_JOINT_IDXS
+            delta = self.djoint * (self.mult if self.in_coarse_mode else 1)
+            if key == keyboard.Key.up:
+                # Raise the proximal joint
+                self.robot.set_joint_angles(
+                    min(cur_prox + delta, Astrobee.JOINT_POS_LIMITS[prox_idx, 1]),
+                    prox_idx,
+                )
+            elif key == keyboard.Key.down:
+                # Lower the proximal joint
+                self.robot.set_joint_angles(
+                    max(cur_prox - delta, Astrobee.JOINT_POS_LIMITS[prox_idx, 0]),
+                    prox_idx,
+                )
+            elif key == keyboard.Key.right:
+                # Raise the distal joint
+                self.robot.set_joint_angles(
+                    min(cur_dist + delta, Astrobee.JOINT_POS_LIMITS[dist_idx, 1]),
+                    dist_idx,
+                )
+            elif key == keyboard.Key.left:
+                # Lower the distal joint
+                self.robot.set_joint_angles(
+                    max(cur_dist - delta, Astrobee.JOINT_POS_LIMITS[dist_idx, 0]),
+                    dist_idx,
+                )
+            # TODO CHANGE THE WAY THIS LOOP IS HANDLED
+            # (Will probably need some multiprocessing)
+            for _ in range(10):
+                pybullet.stepSimulation()
+                time.sleep(1 / 120)
 
 
 if __name__ == "__main__":
