9ae10a9f72ff23341e873150ceb5045281eced99
dan <danielpmorton@gmail.com>
2023-03-29 15:45:26 -0700

    Add safe division helper function and math test cases

--- pyastrobee/utils/math_utils.py
+++ pyastrobee/utils/math_utils.py
@@ -1,6 +1,5 @@
 """Assorted helper functions related to math operations / linear algebra"""
 
-
 import numpy as np
 import numpy.typing as npt
 
@@ -14,7 +13,10 @@ def normalize(vec: npt.ArrayLike) -> np.ndarray:
     Returns:
         np.ndarray: A unit vector in the same direction as the input
     """
-    return np.array(vec) / np.linalg.norm(vec)
+    norm = np.linalg.norm(vec)
+    if abs(norm) < 1e-12:
+        raise ZeroDivisionError("Cannot normalize the vector, it has norm 0")
+    return np.array(vec) / norm
 
 
 def is_diagonal(array: npt.ArrayLike) -> bool:
@@ -27,3 +29,35 @@ def is_diagonal(array: npt.ArrayLike) -> bool:
         bool: True if the array is diagonal, False otherwise
     """
     return np.array_equal(array, np.diag(np.diag(array)))
+
+
+def safe_divide(
+    a: npt.ArrayLike, b: npt.ArrayLike, fill: str = "original", tol: float = 1e-8
+) -> npt.ArrayLike:
+    """Calculates a/b while handling division by zero
+
+    Args:
+        a (npt.ArrayLike): Dividend
+        b (npt.ArrayLike): Divisor (which may contain zeros)
+        fill (str, optional): How to fill the result where the divisor = 0. One of "original" (a/0 = a),
+            "zero" (a/0 = 0), "nan" (a/0 = nan), "inf" (a/0 = inf). Defaults to "original".
+        tol (float, optional): Tolerance for determining float equality with 0. Defaults to 1e-8.
+
+    Returns:
+        npt.ArrayLike: Division result
+    """
+    a = np.array(a, dtype=np.float64)
+    b = np.array(b, dtype=np.float64)
+    fill = fill.lower()
+    if fill == "original":
+        out = a
+    elif fill == "zero":
+        out = np.zeros_like(a)
+    elif fill == "nan":
+        out = np.full(a.shape, np.nan)
+    elif fill == "inf":
+        # Account for sign of dividend for positive or negative inf
+        out = np.array((np.sign(a) + (a == 0)) * np.inf)
+    else:
+        raise ValueError(f"Invalid fill type: {fill}")
+    return np.divide(a, b, out=out, where=np.abs(b) >= tol)
