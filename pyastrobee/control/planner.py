53d71a03a387121fbd0de08f856a2e13b6ae70a8
dan <danielpmorton@gmail.com>
2023-04-03 19:56:15 -0700

    Improve planner formatting

--- pyastrobee/control/planner.py
+++ pyastrobee/control/planner.py
@@ -30,51 +30,97 @@ from pyastrobee.utils.rotations import quaternion_slerp, get_closest_heading_qua
 from pyastrobee.utils.math_utils import normalize
 
 
-def simple_turn_and_move_traj(start_pose, end_pose):
+def point_and_move_pose_traj(
+    start_pose: npt.ArrayLike, end_pose: npt.ArrayLike, n1: int, n2: int, n3: int
+) -> np.ndarray:
+    """Simple pose-only trajectory where the robot points at the goal, moves along a straight line,
+    then aligns with the goal
+
+    Args:
+        start_pose (npt.ArrayLike): Starting position + xyzw quaternion pose, shape (7,)
+        end_pose (npt.ArrayLike): Ending position + xyzw quaternion pose, shape (7,)
+        n1 (int): Number of timesteps for the "point towards goal" motion
+        n2 (int): Number of timesteps for the "move along line" motion
+        n3 (int): Number of timesteps for the "align with goal" motion
+
+    Returns:
+        np.ndarray: Trajectory, shape (n1 + n2 + n3, 7)
+    """
     # Interpolate the orientation, interpolate the position, interpolate the orientation
     start_position = start_pose[:3]
     start_quat = start_pose[3:]
     end_position = end_pose[:3]
     end_quat = end_pose[3:]
     # Trajectory part 1: Maintain same pose, turn to point towards the final position
-    # Need to determine the quaternion pointing towards the end
+    # First, need to find the quaternion to point in the right direction
     heading = normalize(end_position - start_position)
     heading_quat = get_closest_heading_quat(start_quat, heading)
-    n1 = 10
-    traj_1 = fixed_position_traj(start_position, start_quat, heading_quat, n1)
+    traj_1 = fixed_pos_pose_traj(start_position, start_quat, heading_quat, n1)
     # Trajectory part 2: Maintain the same orientation, move to final position
-    n2 = 10
-    traj_2 = fixed_orientation_traj(start_position, end_position, heading_quat, n2)
+    traj_2 = fixed_orn_pose_traj(start_position, end_position, heading_quat, n2)
     # Trajectory part 3: Maintain the same final position, turn to goal orientation
-    n3 = 10
-    traj_3 = fixed_position_traj(end_position, heading_quat, end_quat, n3)
+    traj_3 = fixed_pos_pose_traj(end_position, heading_quat, end_quat, n3)
     # Merge the trajectory components together
-    traj = np.vstack((traj_1, traj_2, traj_3))
-    return traj
+    return np.vstack((traj_1, traj_2, traj_3))
 
 
-def fixed_orientation_traj(pos_1, pos_2, quat, n):
+def fixed_orn_pose_traj(
+    pos_1: npt.ArrayLike, pos_2: npt.ArrayLike, quat: npt.ArrayLike, n: int
+) -> np.ndarray:
+    """Simple pose-only trajectory interpolated between two positions with a fixed orientation
+
+    Args:
+        pos_1 (npt.ArrayLike): Starting XYZ position, shape (3,)
+        pos_2 (npt.ArrayLike): Ending XYZ position, shape (3,)
+        quat (npt.ArrayLike): Fixed orientation (XYZW quaternion), shape (4,)
+        n (int): Number of timesteps
+
+    Returns:
+        np.ndarray: Trajectory, shape (n, 7)
+    """
     positions = np.linspace(pos_1, pos_2, n)
     quats = quat * np.ones((n, 4))
     return np.hstack((positions, quats))
 
 
-def fixed_position_traj(pos, q1, q2, n):
+def fixed_pos_pose_traj(
+    pos: npt.ArrayLike, q1: npt.ArrayLike, q2: npt.ArrayLike, n: int
+) -> np.ndarray:
+    """Simple pose-only trajectory interpolated between two orientations with a fixed position
+
+    Args:
+        pos (npt.ArrayLike): Fixed XYZ position, shape (3,)
+        q1 (npt.ArrayLike): Starting orientation (XYZW quaternion), shape (4,)
+        q2 (npt.ArrayLike): Ending orientation (XYZW quaternion), shape (4,)
+        n (int): Number of timesteps
+
+    Returns:
+        np.ndarray: Trajectory, shape (n, 7)
+    """
     positions = pos * np.ones((n, 3))
     quats = quaternion_slerp(q1, q2, np.linspace(0, 1, n))
     return np.hstack((positions, quats))
 
 
-def simple_interp_traj(start_pose, end_pose, n):
-    # Simplest possible trajectory, just interpolate the position and orientation components
-    # together without accounting for velocity or velocity/accel/force limits
-    pcts = np.linspace(0, 1, n)
+def interpolation_pose_traj(
+    start_pose: npt.ArrayLike, end_pose: npt.ArrayLike, n: int
+) -> np.ndarray:
+    """Simple pose-only trajectory, interpolates between two poses across n timesteps
+
+    Args:
+        start_pose (npt.ArrayLike): Starting position + xyzw quaternion pose, shape (7,)
+        end_pose (npt.ArrayLike): Ending position + xyzw quaternion pose, shape (7,)
+        n (int): Number of timesteps
+
+    Returns:
+        np.ndarray: Trajectory, shape (n, 7)
+    """
     start_position = start_pose[:3]
     start_quat = start_pose[3:]
     end_position = end_pose[:3]
     end_quat = end_pose[3:]
     positions = np.linspace(start_position, end_position, n)
-    quats = quaternion_slerp(start_quat, end_quat, pcts)
+    quats = quaternion_slerp(start_quat, end_quat, np.linspace(0, 1, n))
     return np.hstack((positions, quats))
 
 
