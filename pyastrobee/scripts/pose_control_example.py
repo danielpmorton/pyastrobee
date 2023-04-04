d17b9f9cfe43886c084aea2987bfadd0830086b7
dan <danielpmorton@gmail.com>
2023-04-03 21:26:07 -0700

    Add example of using position control and traj for navigation

---
+++ pyastrobee/scripts/pose_control_example.py
@@ -0,0 +1,43 @@
+"""Quick example of position/constraint control with the Astrobee"""
+
+import time
+
+import numpy as np
+import pybullet
+
+from pyastrobee.control.astrobee import Astrobee
+from pyastrobee.control.controller import PoseController
+from pyastrobee.vision.debug_visualizer import visualize_frame, remove_debug_objects
+from pyastrobee.utils.poses import pos_quat_to_tmat
+
+
+def random_quat():
+    a = np.random.rand(4)
+    return a / np.linalg.norm(a)
+
+
+def random_pose(max_dist=2):
+    p = max_dist * np.random.rand(3)
+    q = random_quat()
+    return np.concatenate((p, q))
+
+
+np.random.seed(0)
+pybullet.connect(pybullet.GUI)
+robot = Astrobee()
+controller = PoseController(robot)
+
+# Use the position control to navigate to various random poses
+for _ in range(10):
+    target = random_pose()
+    ids = visualize_frame(pos_quat_to_tmat(target))
+    controller.go_to_pose(target)
+    print("Done, waiting a few seconds before the next one")
+    time.sleep(2)
+    remove_debug_objects(ids)
+
+# Leave the sim running
+print("Looping sim...")
+while True:
+    pybullet.stepSimulation()
+    time.sleep(1 / 120)
