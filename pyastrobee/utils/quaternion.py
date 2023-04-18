09a0ac1e4f77934a10fcf21eb9a09808dcb58587
dan <danielpmorton@gmail.com>
2023-04-18 10:20:17 -0700

    Add angular velocity determination from quaternions

--- pyastrobee/utils/quaternion.py
+++ pyastrobee/utils/quaternion.py
@@ -7,7 +7,7 @@ q = Quaternion(xyzw=[0.1, 0.2, 0.3, 0.4]) # This will assign values at initializ
 some_pytransform3d_function(q.wxyz) # Pass the wxyz data into modules that use this convention
 """
 
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 import numpy.typing as npt
@@ -125,3 +125,42 @@ def conjugate(quat: npt.ArrayLike) -> np.ndarray:
     """
     x, y, z, w = quat
     return np.array([-x, -y, -z, w])
+
+
+def quats_to_angular_velocities(
+    quats: np.ndarray, dt: Union[float, npt.ArrayLike]
+) -> np.ndarray:
+    """Determines the angular velocities of a sequence of quaternions, for a given sampling time
+
+    Based on AHRS (Attitude and Heading Reference Systems): See ahrs/common/quaternion.py
+
+
+    Args:
+        quats (np.ndarray): Sequence of XYZW quaternions, shape (n, 4)
+        dt (Union[float, np.ndarray]): Sampling time(s). If passing in an array of sampling times,
+            this must be of length (n-1)
+
+    Returns:
+        np.ndarray: Angular velocities (wx, wy, wz), shape (n-1, 3)
+    """
+    xs = quats[:, 0]
+    ys = quats[:, 1]
+    zs = quats[:, 2]
+    ws = quats[:, 3]
+    dw = np.column_stack(
+        [
+            ws[:-1] * xs[1:] - xs[:-1] * ws[1:] - ys[:-1] * zs[1:] + zs[:-1] * ys[1:],
+            ws[:-1] * ys[1:] + xs[:-1] * zs[1:] - ys[:-1] * ws[1:] - zs[:-1] * xs[1:],
+            ws[:-1] * zs[1:] - xs[:-1] * ys[1:] + ys[:-1] * xs[1:] - zs[:-1] * ws[1:],
+        ]
+    )
+    # If dt is scalar, broadcasting is simple. If dt is an array of time deltas,
+    # need to adjust the shape for broadcasting
+    if np.ndim(dt) == 0:
+        return 2.0 * dw / dt
+    else:
+        if len(dt) != dw.shape[0]:
+            raise ValueError(
+                "Invalid dt array size. This needs to be of length (n-1), if we are working with n quaternions"
+            )
+        return 2.0 / np.reshape(dt, (-1, 1)) * dw
