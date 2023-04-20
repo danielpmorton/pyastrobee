e274b8bab542f029c046c0d9775456f4fab86531
dan <danielpmorton@gmail.com>
2023-04-20 13:22:04 -0700

    Add acceleration info to traj, update plotting

--- pyastrobee/control/trajectory.py
+++ pyastrobee/control/trajectory.py
@@ -1,4 +1,7 @@
-"""Trajectory class for Astrobee control, and trajectory-associated helpers"""
+"""Trajectory class for Astrobee control, and trajectory-associated helpers
+
+TODO decide if trajectory attributes should be read-only? (properties)
+"""
 
 from typing import Optional, Union
 
@@ -13,17 +16,19 @@ from pyastrobee.utils.debug_visualizer import visualize_frame
 
 
 class Trajectory:
-    """Trajectory class: Keeps track of a sequence of poses/velocities over a period of time
+    """Trajectory class: Keeps track of a sequence of poses/velocities/accels over a period of time
 
     - All arguments can be omitted as needed (for instance, a pose-only trajectory without velocities,
         or a trajectory without time information)
-    - All positions/orientations/velocities are assumed to be defined in world frame
+    - All positions/orientations/velocities... are assumed to be defined in world frame
 
     Args:
         positions (Optional[npt.ArrayLike]): Sequence of XYZ positions, shape (n, 3)
         quats (Optional[npt.ArrayLike]): Sequence of XYZW quaternions, shape (n, 4)
         lin_vels (Optional[npt.ArrayLike]): Sequence of (vx, vy, vz) linear velocities, shape (n, 3)
         ang_vels (Optional[npt.ArrayLike]): Sequence of (wx, wy, wz) angular velocities, shape (n, 3)
+        lin_accels (Optional[npt.ArrayLike]): Sequence of (ax, ay, az) linear accelerations, shape (n, 3)
+        ang_accels (Optional[npt.ArrayLike]): Sequence of (al_x, al_y, al_z) angular accelerations, shape (n, 3)
         times (Optional[npt.ArrayLike]): Times corresponding to each trajectory entry, shape (n)
     """
 
@@ -33,6 +38,8 @@ class Trajectory:
         quats: Optional[npt.ArrayLike] = None,
         lin_vels: Optional[npt.ArrayLike] = None,
         ang_vels: Optional[npt.ArrayLike] = None,
+        lin_accels: Optional[npt.ArrayLike] = None,
+        ang_accels: Optional[npt.ArrayLike] = None,
         times: Optional[npt.ArrayLike] = None,
     ):
         # TODO decide if there is ever a use-case for not passing in the positions
@@ -44,6 +51,8 @@ class Trajectory:
         self.angular_velocities = (
             np.atleast_2d(ang_vels) if ang_vels is not None else None
         )
+        self.linear_accels = lin_accels if lin_accels is not None else None
+        self.angular_accels = ang_accels if ang_accels is not None else None
         self.times = np.atleast_1d(times) if times is not None else None
         self.num_timesteps = self.positions.shape[0]
 
@@ -60,15 +69,6 @@ class Trajectory:
     def tmats(self) -> np.ndarray:
         """Poses expressed as transformation matrices, shape (n, 4, 4)"""
         return batched_pos_quats_to_tmats(self.poses)
-
-    @property
-    def velocities(self) -> np.ndarray:
-        """Array of linear and angular velocities, shape (n, 6)"""
-        if self.linear_velocities is None:
-            raise ValueError("No linear velocity information available")
-        if self.angular_velocities is None:
-            raise ValueError("No angular velocity information available")
-        return np.column_stack([self.linear_velocities, self.angular_velocities])
 
     def visualize(self) -> None:
         """View the trajectory in Pybullet"""
@@ -104,71 +104,97 @@ def plot_traj(traj: Trajectory, show: bool = True) -> Figure:
         Figure: Matplotlib figure containing the plots
     """
 
-    def _plot_positions(axes: np.ndarray[plt.Axes]):
-        """Helper function to plot position information on the desired axes"""
-        if traj.positions is None:
+    # Indexing helper variables
+    POSITION = 0
+    ORIENTATION = 1
+    VELOCITY = 2
+    ANGULAR_VELOCITY = 3
+    ACCELERATION = 4
+    ANGULAR_ACCEL = 5
+
+    traj_data = {
+        POSITION: traj.positions,
+        ORIENTATION: traj.quaternions,
+        VELOCITY: traj.linear_velocities,
+        ANGULAR_VELOCITY: traj.angular_velocities,
+        ACCELERATION: traj.linear_accels,
+        ANGULAR_ACCEL: traj.angular_accels,
+    }
+
+    traj_labels = {
+        POSITION: ["x", "y", "z"],
+        ORIENTATION: ["qx", "qy", "qz", "qw"],
+        VELOCITY: ["vx", "vy", "vz"],
+        ANGULAR_VELOCITY: ["wx", "wy", "wz"],
+        ACCELERATION: ["ax", "ay", "az"],
+        ANGULAR_ACCEL: ["al_x", "al_y", "al_z"],
+    }
+
+    def _plot(axes: np.ndarray[plt.Axes], component: int):
+        """Helper sub-function for plotting trajectory components
+
+        Args:
+            axes (np.ndarray[plt.Axes]): Matplotlib axes for subplots within a subfigure
+            component (int): Type of trajectory information to plot. One of POSITION, ORIENTATION, VELOCITY, ...
+                (see the indexing helper variables)
+        """
+        # Extract trajectory info and the relevant labels for plotting
+        data = traj_data[component]
+        labels = traj_labels[component]
+        # If the trajectory doesn't contain the info, don't plot it
+        if data is None:
             return
-        labels = ["x", "y", "z"]
+        # Set the x axis based on if we have time information in the trajectory
+        if traj.times is not None:
+            x_axis = traj.times
+            x_label = "Time, s"
+        else:
+            x_axis = range(traj.num_timesteps)
+            x_label = "Timesteps"
+        # Number of components to plot (for instance, position: n = 3: x, y, z)
+        n = data.shape[1]
+        assert n == len(labels)
+        assert n == len(axes)
+        # Plot each component of the trajectory information on a separate axis
         for i, ax in enumerate(axes):
-            ax.plot(x_axis, traj.positions[:, i])
+            ax.plot(x_axis, data[:, i])
             ax.set_title(labels[i])
             ax.set_xlabel(x_label)
-
-    def _plot_quaternions(axes: np.ndarray[plt.Axes]):
-        """Helper function to plot quaternion information on the desired axes"""
-        if traj.quaternions is None:
-            return
-        labels = ["qx", "qy", "qz", "qw"]
-        for i, ax in enumerate(axes):
-            ax.plot(x_axis, traj.quaternions[:, i])
-            ax.set_title(labels[i])
-            ax.set_xlabel(x_label)
-
-    def _plot_linear_velocities(axes: np.ndarray[plt.Axes]):
-        """Helper function to plot linear velocity information on the desired axes"""
-        if traj.linear_velocities is None:
-            return
-        labels = ["vx", "vy", "vz"]
-        for i, ax in enumerate(axes):
-            ax.plot(x_axis, traj.linear_velocities[:, i])
-            ax.set_title(labels[i])
-            ax.set_xlabel(x_label)
-
-    def _plot_angular_velocities(axes: np.ndarray[plt.Axes]):
-        """Helper function to plot angular velocity information on the desired axes"""
-        if traj.angular_velocities is None:
-            return
-        labels = ["wx", "wy", "wz"]
-        for i, ax in enumerate(axes):
-            ax.plot(x_axis, traj.angular_velocities[:, i])
-            ax.set_title(labels[i])
-            ax.set_xlabel(x_label)
-
-    if traj.times is not None:
-        x_axis = traj.times
-        x_label = "Time, s"
-    else:
-        x_axis = range(traj.num_timesteps)
-        x_label = "Timesteps"
 
     fig = plt.figure()
-    # Make an extended version of the plot if we have velocity information
-    if traj.linear_velocities is not None or traj.angular_velocities is not None:
+    # Large plot if we have up second-derivative information
+    if traj.linear_accels is not None or traj.angular_accels is not None:
+        subfigs = fig.subfigures(2, 3)
+        top_left = subfigs[0, 0].subplots(1, 3)
+        _plot(top_left, POSITION)
+        top_middle = subfigs[0, 1].subplots(1, 3)
+        _plot(top_middle, VELOCITY)
+        top_right = subfigs[0, 2].subplots(1, 3)
+        _plot(top_right, ACCELERATION)
+        bot_left = subfigs[1, 0].subplots(1, 4)
+        _plot(bot_left, ORIENTATION)
+        bot_middle = subfigs[1, 1].subplots(1, 3)
+        _plot(bot_middle, ANGULAR_VELOCITY)
+        bot_right = subfigs[1, 2].subplots(1, 3)
+        _plot(bot_right, ANGULAR_ACCEL)
+    # Medium size plot if we only have first-derivative info
+    elif traj.linear_velocities is not None or traj.angular_velocities is not None:
         subfigs = fig.subfigures(2, 2)
         top_left = subfigs[0, 0].subplots(1, 3)
-        _plot_positions(top_left)
+        _plot(top_left, POSITION)
         top_right = subfigs[0, 1].subplots(1, 3)
-        _plot_linear_velocities(top_right)
+        _plot(top_right, VELOCITY)
         bot_left = subfigs[1, 0].subplots(1, 4)
-        _plot_quaternions(bot_left)
+        _plot(bot_left, ORIENTATION)
         bot_right = subfigs[1, 1].subplots(1, 3)
-        _plot_angular_velocities(bot_right)
-    else:  # Only make a section for the position/orientation information
+        _plot(bot_right, ANGULAR_VELOCITY)
+    # Small plot for just plotting position + orientation
+    else:
         subfigs = fig.subfigures(2, 1)
         top = subfigs[0].subplots(1, 3)
-        _plot_positions(top)
+        _plot(top, POSITION)
         bot = subfigs[1].subplots(1, 4)
-        _plot_quaternions(bot)
+        _plot(bot, ORIENTATION)
 
     if show:
         plt.show()
