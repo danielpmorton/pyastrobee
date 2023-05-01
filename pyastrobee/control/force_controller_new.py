Fix quaternion angular error

pyastrobee/control/force_controller_new.py
@@ -19,7 +19,7 @@ from pyastrobee.control.trajectory import (
     stopping_criteria,
 )
 from pyastrobee.control.controller import ControlLogger
-from pyastrobee.utils.quaternions import quaternion_angular_diff
+from pyastrobee.utils.quaternions import quaternion_angular_error
 
 
 class ForcePIDController:
@@ -105,7 +105,7 @@ class ForcePIDController:
         Returns:
             np.ndarray: Torque, (Tx, Ty, Tz), shape (3,)
         """
-        ang_err = quaternion_angular_diff(des_q, cur_q)
+        ang_err = quaternion_angular_error(cur_q, des_q)
         ang_vel_err = cur_w - des_w
         # Standard 3D free-body torque equation based on desired ang. accel and current ang. vel
         torque = self.inertia @ des_a + np.cross(cur_w, self.inertia @ cur_w)

pyastrobee/utils/quaternions.py
@@ -16,8 +16,6 @@ from pyastrobee.utils.rotations import (
     axis_angle_between_two_vectors,
     axis_angle_to_quat,
     quat_to_rmat,
-    quat_to_axis_angle,
-    compact_axis_angle,
 )
 
 
@@ -336,32 +334,37 @@ def quaternion_diff(
     return combine_quaternions(q2, conjugate(q1))
 
 
-def quaternion_angular_diff(
-    q1: Union[Quaternion, npt.ArrayLike], q2: Union[Quaternion, npt.ArrayLike]
+def quaternion_angular_error(
+    q: Union[Quaternion, npt.ArrayLike], q_des: Union[Quaternion, npt.ArrayLike]
 ) -> np.ndarray:
-    """Gives a world-frame compact-axis-angle form of the angular error between two quaternions (q1 -> q2)
+    """Gives the instantaneous angular error between two quaternions (q w.r.t q_des)
 
     - This is similar (but not the same) as a difference between fixed-XYZ conventions
       (for small angles, these are very close).
     - Differences between Euler/Fixed angle sets often do not represent true rotations
 
     Args:
-        q1 (Union[Quaternion, npt.ArrayLike]): Starting XYZW quaternion, shape (4,)
-        q2 (Union[Quaternion, npt.ArrayLike]): Ending XYZW quaternion, shape (4,)
+        q (Union[Quaternion, npt.ArrayLike]): Current XYZW quaternion, shape (4,)
+        q_des (Union[Quaternion, npt.ArrayLike]): Desired XYZW quaternion, shape (4,)
 
     Returns:
-        np.ndarray: XYZW quaternion, shape (4,)
+        np.ndarray: Instantaneous angular error, shape (3,)
     """
-    if isinstance(q1, Quaternion):
-        q1 = q1.xyzw
+    if isinstance(q, Quaternion):
+        q = q.xyzw
     else:
-        q1 = check_quaternion(q1)
-    if isinstance(q2, Quaternion):
-        q2 = q2.xyzw
+        q = check_quaternion(q)
+    if isinstance(q_des, Quaternion):
+        q_des = q_des.xyzw
     else:
-        q2 = check_quaternion(q2)
-    # These lines are based on the math from pytransform3d's quaternion_gradient(),
-    # but simplified to work with just 2 quats and no time info
-    q3 = quaternion_diff(q1, q2)
-    axis, angle = quat_to_axis_angle(q3)
-    return compact_axis_angle(axis, angle)
+        q_des = check_quaternion(q_des)
+
+    x, y, z, w = q
+    xd, yd, zd, wd = q_des
+    return 2 * np.array(
+        [
+            wd * x - xd * w - yd * z + zd * y,
+            wd * y + xd * z - yd * w - zd * x,
+            wd * z - xd * y + yd * x - zd * w,
+        ]
+    )

test/test_quaternions.py
@@ -18,7 +18,7 @@ from pyastrobee.utils.quaternions import (
     xyzw_to_wxyz,
     wxyz_to_xyzw,
     quaternion_diff,
-    quaternion_angular_diff,
+    quaternion_angular_error,
 )
 
 
@@ -83,14 +83,17 @@ class QuaternionTest(unittest.TestCase):
             xyzw_to_wxyz(invalid_quat)
 
     def test_quaternion_diff(self):
+        fixed_angles = np.array([0.01, 0.02, 0.03])
         q1 = np.array([0, 0, 0, 1])
-        q2 = rts.fixed_xyz_to_quat([0.01, 0.02, 0.03])
+        q2 = rts.fixed_xyz_to_quat(fixed_angles)
         q3 = quaternion_diff(q1, q2)
         np.testing.assert_array_almost_equal(q3, q2)
         # Test angular difference in compact axis-angle form
-        ang_diff = quaternion_angular_diff(q1, q2)
+        ang_diff = quaternion_angular_error(q2, q1)
         caa = rts.compact_axis_angle(*rts.quat_to_axis_angle(q2))
-        np.testing.assert_array_almost_equal(ang_diff, caa)
+        np.testing.assert_array_almost_equal(ang_diff, caa, decimal=4)
+        # For small fixed angles, the error should be about the same
+        np.testing.assert_array_almost_equal(ang_diff, fixed_angles, decimal=2)
 
     def test_quaternion_interpolation(self):
         # TODO. Need to think of a good way to evaluate this test case