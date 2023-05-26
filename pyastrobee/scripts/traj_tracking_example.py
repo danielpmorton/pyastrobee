cbab68de9e35f578c27a09f5eb8d63fa571efd60
Daniel Morton <danielpmorton@gmail.com>
2023-05-26 12:55:26 -0700

    PID trajectory tracking example with astrobee

--- pyastrobee/scripts/traj_tracking_example.py
+++ pyastrobee/scripts/traj_tracking_example.py
@@ -6,41 +6,65 @@ This can also be used as a sandbox for experimenting with the PID tuning
 import pybullet
 import numpy as np
 
+from pyastrobee.elements.astrobee import Astrobee
 from pyastrobee.control.force_controller_new import ForcePIDController
 from pyastrobee.utils.bullet_utils import create_box
 from pyastrobee.utils.quaternions import random_quaternion
 from pyastrobee.trajectories.polynomial_trajectories import polynomial_trajectory
-from pyastrobee.trajectories.trajectory import (
-    visualize_traj,
-    compare_trajs,
-    TrajectoryLogger,
-)
+from pyastrobee.trajectories.trajectory import visualize_traj, compare_trajs
 
 
 def box_inertia(m, l, w, h):
     return (1 / 12) * m * np.diag([w**2 + h**2, l**2 + h**2, l**2 + w**2])
 
 
-tracker = TrajectoryLogger()
-pybullet.connect(pybullet.GUI)
-np.random.seed(0)
-pose_1 = [0, 0, 0, 0, 0, 0, 1]
-pose_2 = [1, 2, 3, *random_quaternion()]
-mass = 10
-sidelengths = [0.25, 0.25, 0.25]
-box = create_box(pose_1[:3], pose_1[3:], mass, sidelengths, True)
-max_time = 10
-dt = pybullet.getPhysicsEngineParameters()["fixedTimeStep"]
-traj = polynomial_trajectory(pose_1, pose_2, max_time, dt)
-visualize_traj(traj, 20)
-kp = 20
-kv = 5
-kq = 1
-kw = 0.1
-base_idx = -1  # Base link index of the robot
-inertia = box_inertia(mass, *sidelengths)
-controller = ForcePIDController(box, mass, inertia, kp, kv, kq, kw, dt)
-controller.follow_traj(traj)
-pybullet.disconnect()
-compare_trajs(traj, controller.traj_log)
-controller.control_log.plot()
+def unit_mass_cube_example():
+    pybullet.connect(pybullet.GUI)
+    np.random.seed(0)
+    pose_1 = [0, 0, 0, 0, 0, 0, 1]
+    pose_2 = [1, 2, 3, *random_quaternion()]
+    mass = 10
+    sidelengths = [0.25, 0.25, 0.25]
+    box = create_box(pose_1[:3], pose_1[3:], mass, sidelengths, True)
+    max_time = 10
+    dt = pybullet.getPhysicsEngineParameters()["fixedTimeStep"]
+    traj = polynomial_trajectory(pose_1, pose_2, max_time, dt)
+    visualize_traj(traj, 20)
+    kp = 20
+    kv = 5
+    kq = 1
+    kw = 0.1
+    inertia = box_inertia(mass, *sidelengths)
+    controller = ForcePIDController(box, mass, inertia, kp, kv, kq, kw, dt)
+    controller.follow_traj(traj)
+    pybullet.disconnect()
+    compare_trajs(traj, controller.traj_log)
+    controller.control_log.plot()
+
+
+def astrobee_example():
+    pybullet.connect(pybullet.GUI)
+    np.random.seed(0)
+    pose_1 = [0, 0, 0, 0, 0, 0, 1]
+    pose_2 = [1, 2, 3, *random_quaternion()]
+    robot = Astrobee()
+    max_time = 10
+    dt = pybullet.getPhysicsEngineParameters()["fixedTimeStep"]
+    traj = polynomial_trajectory(pose_1, pose_2, max_time, dt)
+    visualize_traj(traj, 20)
+    kp = 20
+    kv = 5
+    kq = 1
+    kw = 0.1
+    controller = ForcePIDController(
+        robot.id, robot.mass, robot.inertia, kp, kv, kq, kw, dt
+    )
+    controller.follow_traj(traj)
+    pybullet.disconnect()
+    compare_trajs(traj, controller.traj_log)
+    controller.control_log.plot()
+
+
+if __name__ == "__main__":
+    # unit_mass_cube_example()
+    astrobee_example()
