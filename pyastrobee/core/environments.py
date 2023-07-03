3cf55f601c1c29e2992b6ced72127adbf1ebd0e6
Daniel Morton <danielpmorton@gmail.com>
2023-07-03 07:53:26 -0700

    Move make_vec_env into environments file

--- pyastrobee/core/environments.py
+++ pyastrobee/core/environments.py
@@ -8,17 +8,20 @@ TODO
 - Add ability to save/restore state from a state ID (saved in memory) -- ONLY if this is useful
 """
 
-from pathlib import Path
-from typing import Optional, Any
-from datetime import datetime
+import os
 import time
+from pathlib import Path
+from typing import Optional, Any, Callable, Dict, Type, Union
+from datetime import datetime
 
 import pybullet
 import numpy as np
 import numpy.typing as npt
 import gymnasium as gym
 from gymnasium.core import ObsType, ActType
-from stable_baselines3.common.env_util import DummyVecEnv, SubprocVecEnv  # make_vec_env
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.env_util import DummyVecEnv, SubprocVecEnv, VecEnv
+from stable_baselines3.common.vec_env.patch_gym import _patch_env
 
 from pyastrobee.control.force_torque_control import ForceTorqueController
 from pyastrobee.utils.bullet_utils import initialize_pybullet
@@ -29,7 +32,6 @@ from pyastrobee.trajectories.planner import plan_trajectory
 from pyastrobee.trajectories.rewards_and_penalties import deviation_penalty
 from pyastrobee.trajectories.trajectory import Trajectory, visualize_traj
 from pyastrobee.trajectories.sampling import generate_trajs
-from pyastrobee.core.make_vec_env import make_vec_env  # TODO MOVE THIS INTO THIS FILE
 from pyastrobee.utils.debug_visualizer import remove_debug_objects
 
 # Idea: could have a parameter upon initialization of the env to make one the "main"
@@ -362,6 +364,115 @@ class AstrobeeEnv(gym.Env):
             return
         remove_debug_objects(self.debug_viz_ids, self.client)
         self.debug_viz_ids = ()
+
+
+def make_vec_env(
+    env_id: Union[str, Callable[..., gym.Env]],
+    n_envs: int = 1,
+    seed: Optional[int] = None,
+    start_index: int = 0,
+    monitor_dir: Optional[str] = None,
+    wrapper_class: Optional[Callable[[gym.Env], gym.Env]] = None,
+    env_kwargs: Optional[Dict[str, Any]] = None,
+    vec_env_cls: Optional[Type[Union[DummyVecEnv, SubprocVecEnv]]] = None,
+    vec_env_kwargs: Optional[Dict[str, Any]] = None,
+    monitor_kwargs: Optional[Dict[str, Any]] = None,
+    wrapper_kwargs: Optional[Dict[str, Any]] = None,
+    per_env_kwargs: Optional[Dict[int, Dict[str, Any]]] = None,  # NEW
+) -> VecEnv:
+    """Modified version of make_vec_env from Stable Baselines (SB3) to allow for specifying input
+    parameters on a per-environment basis
+
+    The main changes are any lines associated with the per_env_kwargs input. Updated SB3 docstring below:
+
+    Create a wrapped, monitored ``VecEnv``.
+    By default it uses a ``DummyVecEnv`` which is usually faster
+    than a ``SubprocVecEnv``.
+
+    :param env_id: either the env ID, the env class or a callable returning an env
+    :param n_envs: the number of environments you wish to have in parallel
+    :param seed: the initial seed for the random number generator
+    :param start_index: start rank index
+    :param monitor_dir: Path to a folder where the monitor files will be saved.
+        If None, no file will be written, however, the env will still be wrapped
+        in a Monitor wrapper to provide additional information about training.
+    :param wrapper_class: Additional wrapper to use on the environment.
+        This can also be a function with single argument that wraps the environment in many things.
+        Note: the wrapper specified by this parameter will be applied after the ``Monitor`` wrapper.
+        if some cases (e.g. with TimeLimit wrapper) this can lead to undesired behavior.
+        See here for more details: https://github.com/DLR-RM/stable-baselines3/issues/894
+    :param env_kwargs: Optional keyword argument to pass to the env constructor
+    :param vec_env_cls: A custom ``VecEnv`` class constructor. Default: None.
+    :param vec_env_kwargs: Keyword arguments to pass to the ``VecEnv`` class constructor.
+    :param monitor_kwargs: Keyword arguments to pass to the ``Monitor`` class constructor.
+    :param wrapper_kwargs: Keyword arguments to pass to the ``Wrapper`` class constructor.
+    :param per_env_kwargs: Like env-kwargs, keyword arguments fore the env constructor, but keyed to
+        allow for different initialization on a per-env basis via the env's rank. These override any
+        default values set via the env_kwargs input
+    :return: The wrapped environment
+    """
+    env_kwargs = env_kwargs or {}
+    vec_env_kwargs = vec_env_kwargs or {}
+    monitor_kwargs = monitor_kwargs or {}
+    wrapper_kwargs = wrapper_kwargs or {}
+    per_env_kwargs = per_env_kwargs or {}
+    assert vec_env_kwargs is not None  # for mypy
+
+    def make_env(rank: int) -> Callable[[], gym.Env]:
+        def _init() -> gym.Env:
+            # For type checker:
+            assert monitor_kwargs is not None
+            assert wrapper_kwargs is not None
+            assert env_kwargs is not None
+
+            this_env_kwargs = env_kwargs | per_env_kwargs.get(rank, {})
+
+            if isinstance(env_id, str):
+                # if the render mode was not specified, we set it to `rgb_array` as default.
+                kwargs = {"render_mode": "rgb_array"}
+                kwargs.update(this_env_kwargs)
+                try:
+                    env = gym.make(env_id, **kwargs)  # type: ignore[arg-type]
+                except TypeError:
+                    env = gym.make(env_id, **this_env_kwargs)
+            else:
+                env = env_id(**this_env_kwargs)
+                # Patch to support gym 0.21/0.26 and gymnasium
+                env = _patch_env(env)
+
+            if seed is not None:
+                # Note: here we only seed the action space
+                # We will seed the env at the next reset
+                env.action_space.seed(seed + rank)
+            # Wrap the env in a Monitor wrapper
+            # to have additional training information
+            monitor_path = (
+                os.path.join(monitor_dir, str(rank))
+                if monitor_dir is not None
+                else None
+            )
+            # Create the monitor folder if needed
+            if monitor_path is not None and monitor_dir is not None:
+                os.makedirs(monitor_dir, exist_ok=True)
+            env = Monitor(env, filename=monitor_path, **monitor_kwargs)
+            # Optionally, wrap the environment with the provided wrapper
+            if wrapper_class is not None:
+                env = wrapper_class(env, **wrapper_kwargs)
+            return env
+
+        return _init
+
+    # No custom VecEnv is passed
+    if vec_env_cls is None:
+        # Default: use a DummyVecEnv
+        vec_env_cls = DummyVecEnv
+
+    vec_env = vec_env_cls(
+        [make_env(i + start_index) for i in range(n_envs)], **vec_env_kwargs
+    )
+    # Prepare the seeds for the first reset
+    vec_env.seed(seed)
+    return vec_env
 
 
 def test_vec_env():
