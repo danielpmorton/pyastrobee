04ed7121a56a5ed19451739186abab34930910bc
Daniel Morton <danielpmorton@gmail.com>
2023-07-18 19:58:34 -0700

    Add updated vhacd parameters to shell script, plus some notes

--- pyastrobee/scripts/manage_objs.sh
+++ pyastrobee/scripts/manage_objs.sh
@@ -3,10 +3,11 @@
 # TODO:
 # Directories should probably be command-line arguments
 # Should check and throw a warning if the file is not found
+# Delete unnecessary files generated from the process
 
 # (Change the directory / locations of the compiled programs as needed)
 export OBJ_DIR=$HOME/pyastrobee/pyastrobee/assets/meshes/iss/obj
-export VHACD=$HOME/software/v-hacd-4.1.0/app/build/TestVHACD
+export VHACD=$HOME/software/v-hacd/app/build/TestVHACD
 export OBJ2SDF=$HOME/software/bullet3/build_cmake/Extras/obj2sdf/App_obj2sdf
 
 # Go through each directory, find the associated obj file (example: cupola/cupola.obj)
@@ -16,8 +17,9 @@ for dir in $OBJ_DIR/*/
 do
     pushd $dir
     name=${PWD##*/}
-    filename="${name}.obj"
-    $VHACD $filename
+    # Can also use the _minimal version of the objs to neglect lights/handles
+    filename="${name}.obj" 
+    $VHACD $filename -e 0.01 -d 15 -r 200000
     $OBJ2SDF --fileName="$filename"
     popd
 done
