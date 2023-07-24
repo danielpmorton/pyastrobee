85a0281fbca9677e4114e865a2fc7631f51defc2
Daniel Morton <danielpmorton@gmail.com>
2023-07-24 16:03:48 -0700

    Calibrate the safe sets in the ISS

---
+++ pyastrobee/config/iss_safe_boxes.py
@@ -0,0 +1,90 @@
+"""Description of the free space within the ISS as a collection of safe boxes"""
+
+from enum import Enum
+from collections import defaultdict
+
+from pyastrobee.utils.boxes import Box, visualize_3D_box, check_box_intersection
+from pyastrobee.utils.algos import dfs
+
+
+class ISSBox(Enum):
+    """Enumerates and names the safe sets (boxes) inside the ISS"""
+
+    JPM = 0
+    NODE_2 = 1
+    EU_LAB = 2
+    US_LAB = 3
+    NODE_1 = 4
+    # Node 3 has two boxes since there is a toilet blocking an area
+    NODE_3_A = 5
+    NODE_3_B = 6
+    CUPOLA = 7
+    # Corridors between modules
+    JPM_N2_CORRIDOR = 8
+    N2_EU_CORRIDOR = 9
+    N2_US_CORRIDOR = 10
+    US_N1_CORRIDOR = 11
+    N1_N3_CORRIDOR = 12
+    N3_CUPOLA_CORRIDOR = 13
+
+
+# Calibrated values from simulation
+ALL_BOXES = {
+    ISSBox.JPM: Box([3.542, -0.623, -0.739], [10.242, 0.760, 0.749]),
+    ISSBox.NODE_2: Box([-0.761, -2.306, -0.860], [0.831, 0.939, 0.643]),
+    ISSBox.EU_LAB: Box([-7.412, -0.714, -0.775], [-3.770, 0.773, 0.827]),
+    ISSBox.US_LAB: Box([-0.745, -11.113, -0.745], [0.583, -5.958, 0.756]),
+    ISSBox.NODE_1: Box([-0.661, -17.013, -0.832], [0.829, -13.601, 0.747]),
+    ISSBox.NODE_3_A: Box([3.025, -15.437, -0.772], [6.031, -14.932, 0.307]),
+    ISSBox.NODE_3_B: Box([5.249, -15.378, -0.774], [7.355, -14.259, 0.658]),
+    ISSBox.CUPOLA: Box([6.140, -15.028, 1.648], [6.371, -14.437, 2.877]),
+    ISSBox.JPM_N2_CORRIDOR: Box([0.25, -0.338, -0.354], [4.5, 0.325, 0.413]),
+    ISSBox.N2_EU_CORRIDOR: Box([-4.5, -0.338, -0.354], [-0.25, 0.325, 0.413]),
+    ISSBox.N2_US_CORRIDOR: Box([-0.410, -6.75, -0.402], [0.368, -1.5, 0.440]),
+    ISSBox.US_N1_CORRIDOR: Box([-0.410, -14.2, -0.402], [0.368, -10.5, 0.440]),
+    ISSBox.N1_N3_CORRIDOR: Box([0, -15.179, -0.396], [3.889, -14.515, 0.267]),
+    ISSBox.N3_CUPOLA_CORRIDOR: Box([6.021, -15.269, 0.4], [6.771, -14.337, 1.898]),
+}
+
+
+def compute_iss_graph() -> dict[ISSBox, list[ISSBox]]:
+    """Computes the graph between the safe sets in the ISS based on intersecting safe boxes
+
+    Returns:
+        dict[ISSBox, list[ISSBox]]: Adjacency list / graph dictating safe paths in the ISS
+    """
+    n = len(ALL_BOXES)
+    adj = defaultdict(list)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if check_box_intersection(ALL_BOXES[ISSBox(i)], ALL_BOXES[ISSBox(j)]):
+                adj[ISSBox(i)].append(ISSBox(j))
+                adj[ISSBox(j)].append(ISSBox(i))
+    return adj
+
+
+def _show_iss_boxes():
+    """Visualize the safe set in Pybullet"""
+    import pybullet  # pylint: disable=import-outside-toplevel
+
+    pybullet.connect(pybullet.GUI)
+    padding = None  # np.array([0.290513, 0.151942, 0.281129]) / 2
+    for box in ALL_BOXES.values():
+        visualize_3D_box(box, padding)
+    input("Press Enter to exit")
+    pybullet.disconnect()
+
+
+def _test_graph_search():
+    """Example of computing the path between boxes in the ISS"""
+    graph = compute_iss_graph()
+    start = ISSBox.JPM
+    end = ISSBox.CUPOLA
+    path = dfs(graph, ISSBox.JPM, ISSBox.CUPOLA)
+    print(f"Box sequence between {start.name} and {end.name}: ")
+    print(path)
+
+
+if __name__ == "__main__":
+    _test_graph_search()
+    _show_iss_boxes()
