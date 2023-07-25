0d526d083ad368c3acf727dce5cb511b04704cf5
dan <danielpmorton@gmail.com>
2023-07-25 00:00:23 -0700

    Change safe set keying from enum to string

--- pyastrobee/config/iss_safe_boxes.py
+++ pyastrobee/config/iss_safe_boxes.py
@@ -1,65 +1,63 @@
 """Description of the free space within the ISS as a collection of safe boxes"""
 
-from enum import Enum
 from collections import defaultdict
 
 from pyastrobee.utils.boxes import Box, visualize_3D_box, check_box_intersection
 from pyastrobee.utils.algos import dfs
 
-
-class ISSBox(Enum):
-    """Enumerates and names the safe sets (boxes) inside the ISS"""
-
-    JPM = 0
-    NODE_2 = 1
-    EU_LAB = 2
-    US_LAB = 3
-    NODE_1 = 4
-    # Node 3 has two boxes since there is a toilet blocking an area
-    NODE_3_A = 5
-    NODE_3_B = 6
-    CUPOLA = 7
-    # Corridors between modules
-    JPM_N2_CORRIDOR = 8
-    N2_EU_CORRIDOR = 9
-    N2_US_CORRIDOR = 10
-    US_N1_CORRIDOR = 11
-    N1_N3_CORRIDOR = 12
-    N3_CUPOLA_CORRIDOR = 13
-
+# Names of the safe boxes in the ISS
+# Originally these were an enum but this made indexing in the graph search weird
+# Strings are easier so we'll assign them to constants here just for reduced chance of error
+JPM = "jpm"
+NODE_2 = "node_2"
+EU_LAB = "eu_lab"
+US_LAB = "us_lab"
+NODE_1 = "node_1"
+# Node 3 has two boxes since there is a toilet blocking an area
+NODE_3_A = "node_3_a"
+NODE_3_B = "node_3_b"
+CUPOLA = "cupola"
+# Corridors between modules
+JPM_N2_CORRIDOR = "jpm_n2_corridor"
+N2_EU_CORRIDOR = "n2_eu_corridor"
+N2_US_CORRIDOR = "n2_us_corridor"
+US_N1_CORRIDOR = "us_n1_corridor"
+N1_N3_CORRIDOR = "n1_n3_corridor"
+N3_CUPOLA_CORRIDOR = "n3_cupola_corridor"
 
 # Calibrated values from simulation
 ALL_BOXES = {
-    ISSBox.JPM: Box([3.542, -0.623, -0.739], [10.242, 0.760, 0.749]),
-    ISSBox.NODE_2: Box([-0.761, -2.306, -0.860], [0.831, 0.939, 0.643]),
-    ISSBox.EU_LAB: Box([-7.412, -0.714, -0.775], [-3.770, 0.773, 0.827]),
-    ISSBox.US_LAB: Box([-0.745, -11.113, -0.745], [0.583, -5.958, 0.756]),
-    ISSBox.NODE_1: Box([-0.661, -17.013, -0.832], [0.829, -13.601, 0.747]),
-    ISSBox.NODE_3_A: Box([3.025, -15.437, -0.772], [6.031, -14.932, 0.307]),
-    ISSBox.NODE_3_B: Box([5.249, -15.378, -0.774], [7.355, -14.259, 0.658]),
-    ISSBox.CUPOLA: Box([6.140, -15.028, 1.648], [6.371, -14.437, 2.877]),
-    ISSBox.JPM_N2_CORRIDOR: Box([0.25, -0.338, -0.354], [4.5, 0.325, 0.413]),
-    ISSBox.N2_EU_CORRIDOR: Box([-4.5, -0.338, -0.354], [-0.25, 0.325, 0.413]),
-    ISSBox.N2_US_CORRIDOR: Box([-0.410, -6.75, -0.402], [0.368, -1.5, 0.440]),
-    ISSBox.US_N1_CORRIDOR: Box([-0.410, -14.2, -0.402], [0.368, -10.5, 0.440]),
-    ISSBox.N1_N3_CORRIDOR: Box([0, -15.179, -0.396], [3.889, -14.515, 0.267]),
-    ISSBox.N3_CUPOLA_CORRIDOR: Box([6.021, -15.269, 0.4], [6.771, -14.337, 1.898]),
+    JPM: Box([3.542, -0.623, -0.739], [10.242, 0.760, 0.749]),
+    NODE_2: Box([-0.761, -2.306, -0.860], [0.831, 0.939, 0.643]),
+    EU_LAB: Box([-7.412, -0.714, -0.775], [-3.770, 0.773, 0.827]),
+    US_LAB: Box([-0.745, -11.113, -0.745], [0.583, -5.958, 0.756]),
+    NODE_1: Box([-0.661, -17.013, -0.832], [0.829, -13.601, 0.747]),
+    NODE_3_A: Box([3.025, -15.437, -0.772], [6.031, -14.932, 0.307]),
+    NODE_3_B: Box([5.249, -15.378, -0.774], [7.355, -14.259, 0.658]),
+    CUPOLA: Box([6.140, -15.028, 1.648], [6.371, -14.437, 2.877]),
+    JPM_N2_CORRIDOR: Box([0.25, -0.338, -0.354], [4.5, 0.325, 0.413]),
+    N2_EU_CORRIDOR: Box([-4.5, -0.338, -0.354], [-0.25, 0.325, 0.413]),
+    N2_US_CORRIDOR: Box([-0.410, -6.75, -0.402], [0.368, -1.5, 0.440]),
+    US_N1_CORRIDOR: Box([-0.410, -14.2, -0.402], [0.368, -10.5, 0.440]),
+    N1_N3_CORRIDOR: Box([0, -15.179, -0.396], [3.889, -14.515, 0.267]),
+    N3_CUPOLA_CORRIDOR: Box([6.021, -15.269, 0.4], [6.771, -14.337, 1.898]),
 }
 
 
-def compute_iss_graph() -> dict[ISSBox, list[ISSBox]]:
+def compute_iss_graph() -> dict[str, list[str]]:
     """Computes the graph between the safe sets in the ISS based on intersecting safe boxes
 
     Returns:
-        dict[ISSBox, list[ISSBox]]: Adjacency list / graph dictating safe paths in the ISS
+        dict[str, list[str]]: Adjacency list / graph dictating safe paths in the ISS
     """
-    n = len(ALL_BOXES)
+    names = list(ALL_BOXES.keys())
+    n = len(names)
     adj = defaultdict(list)
     for i in range(n):
         for j in range(i + 1, n):
-            if check_box_intersection(ALL_BOXES[ISSBox(i)], ALL_BOXES[ISSBox(j)]):
-                adj[ISSBox(i)].append(ISSBox(j))
-                adj[ISSBox(j)].append(ISSBox(i))
+            if check_box_intersection(ALL_BOXES[names[i]], ALL_BOXES[names[j]]):
+                adj[names[i]].append(names[j])
+                adj[names[j]].append(names[i])
     return adj
 
 
@@ -78,10 +76,10 @@ def _show_iss_boxes():
 def _test_graph_search():
     """Example of computing the path between boxes in the ISS"""
     graph = compute_iss_graph()
-    start = ISSBox.JPM
-    end = ISSBox.CUPOLA
-    path = dfs(graph, ISSBox.JPM, ISSBox.CUPOLA)
-    print(f"Box sequence between {start.name} and {end.name}: ")
+    start = JPM
+    end = CUPOLA
+    path = dfs(graph, start, end)
+    print(f"Box sequence between {start} and {end}: ")
     print(path)
 
 
