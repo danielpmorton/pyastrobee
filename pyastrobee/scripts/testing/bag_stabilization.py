fc6c1ec72ac314fb6ddcfddcbb3ceaed9b2e69bb
Daniel Morton <danielpmorton@gmail.com>
2023-07-27 17:24:12 -0700

    WIP MPC to stabilize the bag at the end of a trajectory

---
+++ pyastrobee/scripts/testing/bag_stabilization.py
@@ -0,0 +1,193 @@
+"""Testing out methods to stabilize the bag at the end of the trajectory
+
+IDEAS
+- MPC with a bunch of possible stabilization trajectories
+- get the current state of the bag (not realistic in the ISS, but a good starting point)
+- use the state of the bag to figure out some sort of proportional/derivative term
+- use the PD term to decide what motion/forces should be applied at the handle location
+- Generate a bunch of possible trajectories to do this
+- Rollout MPC and evaluate which is "best"... best defined by the state of the bag. Maybe add some stuff to the 
+  cost function based on the robot state.. but we can also always disconnect the robot and then stabilize that after
+
+
+
+"""
+
+
+import numpy as np
+import numpy.typing as npt
+from stable_baselines3.common.env_util import DummyVecEnv, SubprocVecEnv
+
+from pyastrobee.core.environments import AstrobeeMPCEnv, make_vec_env
+from pyastrobee.trajectories.trajectory import Trajectory
+from pyastrobee.trajectories.planner import plan_trajectory
+
+
+def bag_stabilization_mpc(
+    duration: float,
+    n_vec_envs: int,
+    debug: bool = False,
+):
+    # Set up main environment
+    main_env = AstrobeeMPCEnv(use_gui=True, is_primary=True)
+    # Set up vectorized environments
+    env_kwargs = {"use_gui": False, "is_primary": False}
+    debug_env_idx = 0
+    # Enable GUI for one of the vec envs if debugging, and use this to test the nominal (non-sampled) trajs
+    per_env_kwargs = {debug_env_idx: {"use_gui": debug, "nominal_rollouts": True}}
+    vec_env = make_vec_env(
+        AstrobeeMPCEnv,
+        n_vec_envs,
+        env_kwargs=env_kwargs,
+        vec_env_cls=SubprocVecEnv if n_vec_envs > 1 else DummyVecEnv,
+        per_env_kwargs=per_env_kwargs,
+    )
+    main_env.reset()
+    vec_env.reset()
+
+    # Give the cargo bag an initial velocity/angular velocity
+    np.random.seed(0)  # Should this be in the environments' seed parameters?
+    lin_vel_stdev = 0.2
+    ang_vel_stdev = 0.1
+    lin_vel = np.random.normal(0, lin_vel_stdev, size=3)
+    ang_vel = np.random.normal(0, ang_vel_stdev, size=3)
+    main_env.client.resetBaseVelocity(main_env.bag.id, lin_vel, ang_vel)
+    bag_ids = vec_env.get_attr("bag.id")
+    clients = vec_env.get_attr("client")
+    for bag_id, client in zip(bag_ids, clients):
+        client.resetBaseVelocity(bag_id, lin_vel, ang_vel)
+
+    # Generate nominal trajectory
+    dt = main_env.client.getPhysicsEngineParameters()["fixedTimeStep"]
+
+    # For the bag stabilization task, plan a trajectory that should help counter the motion of the bag
+    # The MPC might need a different structure overall than the trajectory following task
+    # TODO see if there is a way to generalize the MPC function to serve multiple use cases??
+
+    pos, orn, vel, ang_vel = main_env.robot.dynamics_state
+
+    # Assume that we're working with the top handle bag
+    # If we characterize it as a rigid body we can use the known dimensions to the handle to determine the force
+    # that we'd need to apply to stabilize the rotational motion
+    # Could also model it as a pendulum??
+
+    goal_pos = NotImplemented
+    goal_orn = NotImplemented
+    goal_vel = NotImplemented
+    goal_ang_vel = NotImplemented
+
+    nominal_traj = plan_trajectory(
+        pos,
+        orn,
+        vel,
+        ang_vel,
+        np.zeros(3),
+        np.zeros(3),
+        goal_pose[:3],
+        goal_pose[3:],
+        np.zeros(3),
+        np.zeros(3),
+        np.zeros(3),
+        np.zeros(3),
+        duration,
+        dt,
+    )
+
+    # Time parameters (TODO make some of these inputs?)
+    cur_time = 0.0
+    traj_end_time = nominal_traj.times[-1]
+    max_stopping_time = 3  # seconds
+    max_time = traj_end_time + max_stopping_time
+    target_rollout_duration = 1 / 3  # seconds
+
+    # Init stopping mode for when we get to the end of the trajectory
+    stopping = False
+
+    # Execute the main MPC code in a try/finally block to make sure things close out / clean up when done
+    try:
+        while True:
+            # Determine the duration of the rollout and where on the trajectory we are interested
+            remaining_traj_time = traj_end_time - cur_time
+            remaining_total_time = max_time - cur_time
+            stopping = remaining_traj_time <= dt
+            done = remaining_total_time <= dt
+            if done:
+                print("Terminating due to time limit")
+                break
+            if stopping:
+                rollout_duration = min(target_rollout_duration, remaining_total_time)
+                lookahead_idx = -1
+            else:  # Following the trajectory
+                rollout_duration = min(target_rollout_duration, remaining_traj_time)
+                # Handle the case where we are nearing the end of the trajectory
+                if remaining_traj_time < target_rollout_duration:
+                    lookahead_idx = -1
+                else:
+                    lookahead_idx = np.searchsorted(
+                        nominal_traj.times, cur_time + rollout_duration
+                    )
+            # Clear any previously visualized trajectories before viewing the new plan
+            if debug:
+                vec_env.env_method("unshow_traj_plan", indices=[debug_env_idx])
+
+            # Ensure that the vec envs start from the same point as the main simulation
+            saved_file = main_env.save_state()
+            vec_env.env_method("restore_state", saved_file)
+            # Set the desired state of the robot at the lookahead point
+            target_state = [
+                nominal_traj.positions[lookahead_idx],
+                nominal_traj.quaternions[lookahead_idx],
+                nominal_traj.linear_velocities[lookahead_idx],
+                nominal_traj.angular_velocities[lookahead_idx],
+                nominal_traj.linear_accels[lookahead_idx],
+                nominal_traj.angular_accels[lookahead_idx],
+                rollout_duration,
+            ]
+            main_env.set_target_state(*target_state)
+            vec_env.env_method("set_target_state", *target_state)
+            # Generate sampled trajectories within each vec env
+            vec_env.env_method("sample_trajectory")
+            if debug:
+                vec_env.env_method("show_traj_plan", 10, indices=[debug_env_idx])
+            # Stepping in the vec env will follow the sampled trajectory
+            # Action input in step(actions) is a dummy parameter for now, just for Gym compatibility
+            observation, reward, done, info = vec_env.step(np.zeros(n_vec_envs))
+            best_traj: Trajectory = vec_env.get_attr(
+                "traj_plan", [int(np.argmax(reward))]
+            )[0]
+            # Follow the best rollout in the main environment. (Use dummy action value in step call)
+            main_env.traj_plan = best_traj
+            observation, reward, terminated, truncated, info = main_env.step(0)
+
+            # TODO: use stopping criteria in terminated/truncated
+            # Idea: terminated -> successfully stopped; truncated -> out of time
+            # if terminated: print_green(success_message); elif truncated: print_red(failure_message); then break
+
+            # Update our knowledge of the last acceleration commands
+            main_env.last_accel_cmd = best_traj.linear_accels[-1]
+            main_env.last_alpha_cmd = best_traj.angular_accels[-1]
+            vec_env.set_attr("last_accel_cmd", best_traj.linear_accels[-1])
+            vec_env.set_attr("last_alpha_cmd", best_traj.angular_accels[-1])
+
+            # Update our time information
+            cur_time += rollout_duration
+
+        input("Complete. Press Enter to exit")
+    finally:
+        print("Closing environments")
+        main_env.close()
+        vec_env.close()
+
+
+def _test_bag_stabilization():
+    """Quick function to test that the parallel MPC is working as expected"""
+    start_pose = [0, 0, 0, 0, 0, 0, 1]
+    end_pose = [6, 0, 0.2, 0, 0, 0, 1]  # Easy-to-reach location in JPM
+    duration = 5
+    n_vec_envs = 5
+    debug = True
+    bag_stabilization_mpc(start_pose, end_pose, duration, n_vec_envs, debug)
+
+
+if __name__ == "__main__":
+    _test_bag_stabilization()
