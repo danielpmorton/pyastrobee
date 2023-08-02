e7b8e2432f84e89f2fef7b8bd04bb8ded07b2c34
dan <danielpmorton@gmail.com>
2023-08-01 20:51:47 -0700

    Rigid cargo bag URDF loads and attaches to robot, TOP HANDLE ONLY

--- pyastrobee/core/rigid_bag.py
+++ pyastrobee/core/rigid_bag.py
@@ -1,14 +1,14 @@
-# """Class for handling loading the cargo bag and managing attributes such as physical properties
+"""Rigid version of the cargo bag, using joints in the URDF to mimic what we would see from a deformable
 
-# TODO !! Fix the softbody velocity issue
-# TODO ! the unloading mechanic is currently broken since it only removes the softbody and not the visual
-# TODO If the reliability of the softbody position/orientation is not good, use the get_bag_frame() function I made
-# TODO decide if the bag_props import can be handled better
-# TODO decide if the constants should be moved to class attributes
-# TODO decide if we should anchor to the gripper fingers or the arm distal link (currently distal)
-# TODO decide if we should set up the class attributes to handle multiple bags loaded in sim
-#     (Like the Astrobee class)
-# """
+TODO add support for more than just the top handle bag!!!
+TODO tune the position control force on the handle
+
+Additional relevant TODOs from the original CargoBag file:
+TODO decide if the bag_props import can be handled better
+TODO decide if the constants should be moved to class attributes
+TODO decide if we should anchor to the gripper fingers or the arm distal link (currently distal)
+"""
+
 import time
 from typing import Union, Optional
 
@@ -19,29 +19,17 @@ import numpy.typing as npt
 
 import pyastrobee.config.bag_properties as bag_props
 from pyastrobee.core.astrobee import Astrobee
-from pyastrobee.utils.bullet_utils import (
-    load_deformable_object,
-    create_sphere,
-    initialize_pybullet,
-)
-from pyastrobee.utils.mesh_utils import get_mesh_data, get_closest_mesh_vertex
+from pyastrobee.utils.bullet_utils import initialize_pybullet
 from pyastrobee.utils.poses import pos_quat_to_tmat, tmat_to_pos_quat
-from pyastrobee.utils.python_utils import print_green, flatten
-from pyastrobee.utils.quaternions import quats_to_angular_velocities
+from pyastrobee.utils.python_utils import print_green
 from pyastrobee.utils.transformations import invert_transform_mat, make_transform_mat
 from pyastrobee.utils.rotations import quat_to_rmat, Ry, rmat_to_quat
 from pyastrobee.utils.dynamics import box_inertia
-from pyastrobee.utils.debug_visualizer import visualize_points
 
 # Constants. TODO Move these to class attributes?
-# MESH_DIR = "pyastrobee/assets/meshes/bags/"
 SINGLE_HANDLE_BAGS = ["front_handle", "right_handle", "top_handle"]
 DUAL_HANDLE_BAGS = ["front_back_handle", "right_left_handle", "top_bottom_handle"]
 BAG_NAMES = SINGLE_HANDLE_BAGS + DUAL_HANDLE_BAGS
-# _objs = [MESH_DIR + name + ".obj" for name in BAG_NAMES]
-# _vtks = [MESH_DIR + name + ".vtk" for name in BAG_NAMES]
-# OBJS = dict(zip(BAG_NAMES, _objs))
-# VTKS = dict(zip(BAG_NAMES, _vtks))
 HANDLE_TRANSFORMS = {
     "front": bag_props.FRONT_HANDLE_TRANSFORM,
     "back": bag_props.BACK_HANDLE_TRANSFORM,
@@ -50,32 +38,19 @@ HANDLE_TRANSFORMS = {
     "top": bag_props.TOP_HANDLE_TRANSFORM,
     "bottom": bag_props.BOTTOM_HANDLE_TRANSFORM,
 }
-# BAG_CORNERS = dict(
-#     zip(
-#         BAG_NAMES,
-#         [
-#             bag_props.FRONT_HANDLE_BAG_CORNERS,
-#             bag_props.RIGHT_HANDLE_BAG_CORNERS,
-#             bag_props.TOP_HANDLE_BAG_CORNERS,
-#             bag_props.FRONT_BACK_HANDLE_BAG_CORNERS,
-#             bag_props.RIGHT_LEFT_HANDLE_BAG_CORNERS,
-#             bag_props.TOP_BOTTOM_HANDLE_BAG_CORNERS,
-#         ],
-#     )
-# )
 
 
 class RigidCargoBag:
-    # """Class for loading and managing properties associated with the cargo bags
+    """Class for loading and managing properties associated with the rigid URDF-based cargo bags
 
-    # Args:
-    #     bag_name (str): Type of cargo bag to load. Single handle: "front_handle", "right_handle", "top_handle".
-    #         Dual handle: "front_back_handle", "right_left_handle", "top_bottom_handle"
-    #     pos (npt.ArrayLike, optional): Initial XYZ position to load the bag. Defaults to (0, 0, 0)
-    #     orn (npt.ArrayLike, optional): Initial XYZW quaternion to load the bag. Defaults to (0, 0, 0, 1)
-    #     client (BulletClient, optional): If connecting to multiple physics servers, include the client
-    #         (the class instance, not just the ID) here. Defaults to None (use default connected client)
-    # """
+    Args:
+        bag_name (str): Type of cargo bag to load. Single handle: "front_handle", "right_handle", "top_handle".
+            Dual handle: "front_back_handle", "right_left_handle", "top_bottom_handle"
+        pos (npt.ArrayLike, optional): Initial XYZ position to load the bag. Defaults to (0, 0, 0)
+        orn (npt.ArrayLike, optional): Initial XYZW quaternion to load the bag. Defaults to (0, 0, 0, 1)
+        client (BulletClient, optional): If connecting to multiple physics servers, include the client
+            (the class instance, not just the ID) here. Defaults to None (use default connected client)
+    """
 
     URDF = "pyastrobee/assets/urdf/rigid_bag.urdf"
     LINKS_PER_HANDLE = 4  # 3 dummy links for roll/pitch/yaw, plus the handle itself
@@ -97,21 +72,7 @@ class RigidCargoBag:
             raise ValueError(
                 f"Invalid bag name: {bag_name}. Must be one of {BAG_NAMES}"
             )
-
         self.id = self.client.loadURDF(RigidCargoBag.URDF, pos, orn)
-        # TODO UPDATE THIS!!! ("unlock" the joints)
-        # pybullet.setJointMotorControlArray(
-        #     self.id, [0, 1, 2], pybullet.VELOCITY_CONTROL, forces=[0.1, 0.1, 0.1]
-        # )
-        # This will cause the handle to spring back into its natural position
-        pybullet.setJointMotorControlArray(
-            self.id,
-            [0, 1, 2],
-            pybullet.POSITION_CONTROL,
-            [0, 0, 0],
-            forces=[0.1, 0.1, 0.1],
-        )
-
         self._dt = self.client.getPhysicsEngineParameters()["fixedTimeStep"]
         self.mass = 5  # kg
         # This inertia is slightly approximate because some mass is in the handle and dummy links
@@ -119,20 +80,27 @@ class RigidCargoBag:
         self._name = bag_name
         # Initializations
         self._constraints = {}
-        self._anchor_objects = {}
-        # self._mesh_vertices = None
-        # self._num_mesh_vertices = None
         self._attached = []
-        self._id = None
+        # Add position control to the handle(s) so its springs back into its natural position
+        # to provide some resistance to motion like we would see in a deformable
+        # The dummy joints are the joints associated with the motion of the handle
+        if self.num_handles == 1:
+            dummy_joint_ids = [0, 1, 2]
+        elif self.num_handles == 2:
+            dummy_joint_ids = [0, 1, 2, 4, 5, 6]
+        pybullet.setJointMotorControlArray(
+            self.id,
+            dummy_joint_ids,
+            pybullet.POSITION_CONTROL,
+            [0] * len(dummy_joint_ids),
+            forces=[0.1] * len(dummy_joint_ids),  # TODO tune force
+        )
         print_green("Bag is ready")
 
     @property
-    def anchors(self) -> tuple[list[int], list[int]]:
-        """Anchor IDs and IDs of their associated visual geometries"""
-        # Unpack the list of lists in the anchor dictionaries
-        return flatten(self._constraints.values()), flatten(
-            self._anchor_objects.values()
-        )
+    def constraints(self) -> list[int]:
+        """Active IDs of constraints on the bag"""
+        return list(self._constraints.values())
 
     @property
     def attached(self) -> list[int]:
@@ -313,110 +281,65 @@ class RigidCargoBag:
         Args:
             robot (Astrobee): Robot to attach
         """
-        # Generate the constraints between the bag and the robot: First, find the points on the mesh on either side
-        # of the handle (Using the left/right gripper link frames as reference points), then create the anchors
-        # pos_1 = robot.get_link_transform(robot.Links.GRIPPER_LEFT_DISTAL)[:3, 3]
-        # pos_2 = robot.get_link_transform(robot.Links.GRIPPER_RIGHT_DISTAL)[:3, 3]
-        # v1_pos, v1_id = get_closest_mesh_vertex(pos_1, self.mesh_vertices)
-        # v2_pos, v2_id = get_closest_mesh_vertex(pos_2, self.mesh_vertices)
-        # # handle_pos = self.get_handle_transform(0)[:3, 3] # in world
-
-        # return  # REMOVE THIS
-
-        HANDLE_LINK_IDX = 3  # TODO FIX THIS
+        handle_link_index = self.num_handles * self.LINKS_PER_HANDLE - 1
         cid = self.client.createConstraint(
             robot.id,
             robot.Links.ARM_DISTAL.value,
             self.id,
-            HANDLE_LINK_IDX,
-            pybullet.JOINT_FIXED,  # Point to point
+            handle_link_index,
+            pybullet.JOINT_FIXED,
             [0, 0, 1],
             robot.TRANSFORMS.GRIPPER_TO_ARM_DISTAL[:3, 3],
             [0, 0, 0],
-            [0, 0, 0, 1],
-            rmat_to_quat(Ry(-np.pi / 2)),
+            # These frame orientations (below) align the grasp frame to the handle link frame
+            [0, 0, 0, 1],  # Robot link frame orientation
+            rmat_to_quat(Ry(-np.pi / 2)),  # Bag handle frame orientation
         )
-
-        # # Create a collision-less sphere to visualize the anchor position
-        # geom_pos = robot.ee_pose
-        # geom_id = create_sphere(
-        #     geom_pos, 0.01, 0.01, False, [0, 1, 0, 0.5], self.client
-        # )
-        # # Then create a secondary anchor to make sure this sphere stays in the right place
-        # geom_constraint_id = self.client.createConstraint(
-        #     robot.id,
-        #     robot.Links.ARM_DISTAL.value,
-        #     geom_id,
-        #     -1,
-        #     pybullet.JOINT_FIXED,
-        #     [0, 0, 0],
-        #     robot.TRANSFORMS.ARM_DISTAL_TO_GRIPPER[:3, 3],
-        #     [0, 0, 0],
-        # )
-
-        # TODO get the geom in here
         self._constraints.update({robot.id: cid})
-        # self._anchors.update({robot.id: [anchor1_id, anchor2_id]})
-        # self._anchor_objects.update({robot.id: [geom1_id, geom2_id]})
-        # self._attached.append(robot.id)
+        self._attached.append(robot.id)
 
     def get_handle_transform(self, handle_index: int = 0) -> np.ndarray:
-        # """Calculates the transformation matrix (w.r.t the world) for a specified link
+        """Calculates the transformation matrix (w.r.t the world) for a specified handle
 
-        # Args:
-        #     link_index (int): Index of the link on the robot
+        Args:
+            handle_index (int): Index of the handle on the bag
 
-        # Returns:
-        #     np.ndarray: Transformation matrix (link to world). Shape = (4,4)
-        # """
-
-        if handle_index == 0:
-            # Three dummy links for roll, pitch, yaw (0, 1, and 2)
-            # The handle is the 4th link (index 3)
-            link_index = RigidCargoBag.LINKS_PER_HANDLE - 1
-
+        Returns:
+            np.ndarray: Transformation matrix (handle to world). Shape = (4,4)
+        """
+        if (handle_index + 1) > self.num_handles:
+            raise ValueError(
+                f"Invalid handle index: {handle_index}. Bag only has {self.num_handles} handles"
+            )
+        handle_link_index = (handle_index + 1) * self.LINKS_PER_HANDLE - 1
         link_state = self.client.getLinkState(
-            self.id, link_index, computeForwardKinematics=True
+            self.id, handle_link_index, computeForwardKinematics=True
         )
-        # First two link state values are linkWorldPosition, linkWorldOrientation
-        # There are other state positions and orientations, but they're confusing. (TODO check on these)
         pos, quat = link_state[:2]
         return make_transform_mat(quat_to_rmat(quat), pos)
 
     def detach(self) -> None:
-        """Remove all anchors (and visuals) from the bag"""
-
-        anchors, anchor_objects = self.anchors
-        for cid in anchors:
+        """Remove all constraints from the bag"""
+        for cid in self.constraints:
             self.client.removeConstraint(cid)
-        for obj in anchor_objects:
-            self.client.removeBody(obj)
         self._constraints = {}
-        self._anchor_objects = {}
         self._attached = []
 
     def detach_robot(self, robot_id: int) -> None:
-        """Detaches a specific robot from the bag by removing its associated anchors / visuals
+        """Detaches a specific robot from the bag by removing its associated constraint
 
         Args:
             robot_id (int): Pybullet ID of the robot to detach
         """
         if robot_id not in self.attached:
             raise ValueError("Cannot detach robot: ID unknown")
-        for cid in self._constraints[robot_id]:
-            self.client.removeConstraint(cid)
-        for obj in self._anchor_objects[robot_id]:
-            self.client.removeBody(obj)
+        self.client.removeConstraint(self._constraints[robot_id])
         self._constraints.pop(robot_id)
-        self._anchor_objects.pop(robot_id)
         self._attached.remove(robot_id)
 
     def unload(self) -> None:
-        """Removes the cargo bag from the simulation
-
-        TODO this is kinda broken since it only removes the simulated body rather than the visual
-        (if we update how we load the texture for the softbody this might be fixed)
-        """
+        """Removes the cargo bag from the simulation"""
+        self.detach()
         self.client.removeBody(self.id)
         self.id = None
 
@@ -438,7 +361,7 @@ class RigidCargoBag:
 
 def _main():
     # Very simple example of loading the bag and attaching a robot
-    client = initialize_pybullet(bg_color=(1, 0.8, 0.8))
+    client = initialize_pybullet(bg_color=(0.8, 0.8, 1))
     robot = Astrobee()
     bag = RigidCargoBag("top_handle")
     bag.attach_to(robot)
