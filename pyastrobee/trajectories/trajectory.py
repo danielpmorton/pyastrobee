cdc998c351226bc287b4afc8fea8a1e2c39e02f5
Daniel Morton <danielpmorton@gmail.com>
2023-08-15 12:34:13 -0700

    Simplify plotting logic

--- pyastrobee/trajectories/trajectory.py
+++ pyastrobee/trajectories/trajectory.py
@@ -1,7 +1,6 @@
 """Trajectory class for Astrobee control, and trajectory-associated helpers
 
 TODO decide if trajectory attributes should be read-only? (properties)
-TODO simplify the plotting code (a lot of repeated code between single-traj plotting and 2-traj plotting)
 TODO make a subclass of Trajectory with the log() method
 TODO make it clearer what happens if some of the traj components are None
      (Numerically calculate the gradient? Remove the option to set these as None?)
@@ -272,41 +271,21 @@ def plot_traj(traj: Trajectory, show: bool = True, fmt: str = "k-") -> Figure:
     else:
         x_axis = traj.times
         x_label = "Time, s"
-    # fmt: off
-    # Large plot if we have up second-derivative information
-    if traj.linear_accels is not None or traj.angular_accels is not None:
-        subfigs = fig.subfigures(2, 3)
-        top_left = subfigs[0, 0].subplots(1, 3)
-        _plot(top_left, traj.positions, labels[POS], x_axis, x_label, fmt)
-        top_middle = subfigs[0, 1].subplots(1, 3)
-        _plot(top_middle, traj.linear_velocities, labels[LIN_VEL], x_axis, x_label, fmt)
-        top_right = subfigs[0, 2].subplots(1, 3)
-        _plot(top_right, traj.linear_accels, labels[LIN_ACCEL], x_axis, x_label, fmt)
-        bot_left = subfigs[1, 0].subplots(1, 4)
-        _plot(bot_left, traj.quaternions, labels[ORN], x_axis, x_label, fmt)
-        bot_middle = subfigs[1, 1].subplots(1, 3)
-        _plot(bot_middle, traj.angular_velocities, labels[ANG_VEL], x_axis, x_label, fmt)
-        bot_right = subfigs[1, 2].subplots(1, 3)
-        _plot(bot_right, traj.angular_accels, labels[ANG_ACCEL], x_axis, x_label, fmt)
-    # Medium size plot if we only have first-derivative info
-    elif traj.linear_velocities is not None or traj.angular_velocities is not None:
-        subfigs = fig.subfigures(2, 2)
-        top_left = subfigs[0, 0].subplots(1, 3)
-        _plot(top_left, traj.positions, labels[POS], x_axis, x_label, fmt)
-        top_right = subfigs[0, 1].subplots(1, 3)
-        _plot(top_right, traj.linear_velocities, labels[LIN_VEL], x_axis, x_label, fmt)
-        bot_left = subfigs[1, 0].subplots(1, 4)
-        _plot(bot_left, traj.quaternions, labels[ORN], x_axis, x_label, fmt)
-        bot_right = subfigs[1, 1].subplots(1, 3)
-        _plot(bot_right, traj.angular_velocities, labels[ANG_VEL], x_axis, x_label, fmt)
-    # Small plot for just plotting position + orientation
-    else:
-        subfigs = fig.subfigures(2, 1)
-        top = subfigs[0].subplots(1, 3)
-        _plot(top, traj.positions, labels[POS], x_axis, x_label, fmt)
-        bot = subfigs[1].subplots(1, 4)
-        _plot(bot, traj.quaternions, labels[ORN], x_axis, x_label, fmt)
-    # fmt: on
+    # Top row is position info, bottom row is orientation info
+    # Columns give derivative info
+    subfigs = fig.subfigures(2, 3)
+    top_left = subfigs[0, 0].subplots(1, 3)
+    _plot(top_left, traj.positions, labels[POS], x_axis, x_label, fmt)
+    top_middle = subfigs[0, 1].subplots(1, 3)
+    _plot(top_middle, traj.linear_velocities, labels[LIN_VEL], x_axis, x_label, fmt)
+    top_right = subfigs[0, 2].subplots(1, 3)
+    _plot(top_right, traj.linear_accels, labels[LIN_ACCEL], x_axis, x_label, fmt)
+    bot_left = subfigs[1, 0].subplots(1, 4)
+    _plot(bot_left, traj.quaternions, labels[ORN], x_axis, x_label, fmt)
+    bot_middle = subfigs[1, 1].subplots(1, 3)
+    _plot(bot_middle, traj.angular_velocities, labels[ANG_VEL], x_axis, x_label, fmt)
+    bot_right = subfigs[1, 2].subplots(1, 3)
+    _plot(bot_right, traj.angular_accels, labels[ANG_ACCEL], x_axis, x_label, fmt)
     if show:
         plt.show()
     return fig
@@ -397,52 +376,28 @@ def compare_trajs(
         x_axis_2 = range(traj_2.num_timesteps)
     else:
         x_axis_2 = traj_2.times
-    # Large plot if we have up second-derivative information
+    # Top row is position info, bottom row is orientation info
+    # Columns give derivative info
     # fmt: off
-    if traj_1.linear_accels is not None or traj_1.angular_accels is not None:
-        subfigs = fig.subfigures(2, 3)
-        top_left = subfigs[0, 0].subplots(1, 3)
-        _plot(top_left, traj_1.positions, labels[POS], x_axis_1, x_label, fmt_1)
-        _plot(top_left, traj_2.positions, labels[POS], x_axis_2, x_label, fmt_2)
-        top_middle = subfigs[0, 1].subplots(1, 3)
-        _plot(top_middle, traj_1.linear_velocities, labels[LIN_VEL], x_axis_1, x_label, fmt_1)
-        _plot(top_middle, traj_2.linear_velocities, labels[LIN_VEL], x_axis_2, x_label, fmt_2)
-        top_right = subfigs[0, 2].subplots(1, 3)
-        _plot(top_right, traj_1.linear_accels, labels[LIN_ACCEL], x_axis_1, x_label, fmt_1)
-        _plot(top_right, traj_2.linear_accels, labels[LIN_ACCEL], x_axis_2, x_label, fmt_2)
-        bot_left = subfigs[1, 0].subplots(1, 4)
-        _plot(bot_left, traj_1.quaternions, labels[ORN], x_axis_1, x_label, fmt_1)
-        _plot(bot_left, traj_2.quaternions, labels[ORN], x_axis_2, x_label, fmt_2)
-        bot_mid = subfigs[1, 1].subplots(1, 3)
-        _plot(bot_mid, traj_1.angular_velocities, labels[ANG_VEL], x_axis_1, x_label, fmt_1)
-        _plot(bot_mid, traj_2.angular_velocities, labels[ANG_VEL], x_axis_2, x_label, fmt_2)
-        bot_right = subfigs[1, 2].subplots(1, 3)
-        _plot(bot_right, traj_1.angular_accels, labels[ANG_ACCEL], x_axis_1, x_label, fmt_1)
-        _plot(bot_right, traj_2.angular_accels, labels[ANG_ACCEL], x_axis_2, x_label, fmt_2)
-    # Medium size plot if we only have first-derivative info
-    elif traj_1.linear_velocities is not None or traj_1.angular_velocities is not None:
-        subfigs = fig.subfigures(2, 2)
-        top_left = subfigs[0, 0].subplots(1, 3)
-        _plot(top_left, traj_1.positions, labels[POS], x_axis_1, x_label, fmt_1)
-        _plot(top_left, traj_2.positions, labels[POS], x_axis_2, x_label, fmt_2)
-        top_right = subfigs[0, 1].subplots(1, 3)
-        _plot(top_right, traj_1.linear_velocities, labels[LIN_VEL], x_axis_1, x_label, fmt_1)
-        _plot(top_right, traj_2.linear_velocities, labels[LIN_VEL], x_axis_2, x_label, fmt_2)
-        bot_left = subfigs[1, 0].subplots(1, 4)
-        _plot(bot_left, traj_1.quaternions, labels[ORN], x_axis_1, x_label, fmt_1)
-        _plot(bot_left, traj_2.quaternions, labels[ORN], x_axis_2, x_label, fmt_2)
-        bot_right = subfigs[1, 1].subplots(1, 3)
-        _plot(bot_right, traj_1.angular_velocities, labels[ANG_VEL], x_axis_1, x_label, fmt_1)
-        _plot(bot_right, traj_2.angular_velocities, labels[ANG_VEL], x_axis_2, x_label, fmt_2)
-    # Small plot for just plotting position + orientation
-    else:
-        subfigs = fig.subfigures(2, 1)
-        top = subfigs[0].subplots(1, 3)
-        _plot(top, traj_1.positions, labels[POS], x_axis_1, x_label, fmt_1)
-        _plot(top, traj_2.positions, labels[POS], x_axis_2, x_label, fmt_2)
-        bot = subfigs[1].subplots(1, 4)
-        _plot(bot, traj_1.quaternions, labels[ORN], x_axis_1, x_label, fmt_1)
-        _plot(bot, traj_2.quaternions, labels[ORN], x_axis_2, x_label, fmt_2)
+    subfigs = fig.subfigures(2, 3)
+    top_left = subfigs[0, 0].subplots(1, 3)
+    _plot(top_left, traj_1.positions, labels[POS], x_axis_1, x_label, fmt_1)
+    _plot(top_left, traj_2.positions, labels[POS], x_axis_2, x_label, fmt_2)
+    top_middle = subfigs[0, 1].subplots(1, 3)
+    _plot(top_middle, traj_1.linear_velocities, labels[LIN_VEL], x_axis_1, x_label, fmt_1)
+    _plot(top_middle, traj_2.linear_velocities, labels[LIN_VEL], x_axis_2, x_label, fmt_2)
+    top_right = subfigs[0, 2].subplots(1, 3)
+    _plot(top_right, traj_1.linear_accels, labels[LIN_ACCEL], x_axis_1, x_label, fmt_1)
+    _plot(top_right, traj_2.linear_accels, labels[LIN_ACCEL], x_axis_2, x_label, fmt_2)
+    bot_left = subfigs[1, 0].subplots(1, 4)
+    _plot(bot_left, traj_1.quaternions, labels[ORN], x_axis_1, x_label, fmt_1)
+    _plot(bot_left, traj_2.quaternions, labels[ORN], x_axis_2, x_label, fmt_2)
+    bot_mid = subfigs[1, 1].subplots(1, 3)
+    _plot(bot_mid, traj_1.angular_velocities, labels[ANG_VEL], x_axis_1, x_label, fmt_1)
+    _plot(bot_mid, traj_2.angular_velocities, labels[ANG_VEL], x_axis_2, x_label, fmt_2)
+    bot_right = subfigs[1, 2].subplots(1, 3)
+    _plot(bot_right, traj_1.angular_accels, labels[ANG_ACCEL], x_axis_1, x_label, fmt_1)
+    _plot(bot_right, traj_2.angular_accels, labels[ANG_ACCEL], x_axis_2, x_label, fmt_2)
     # fmt: on
     if show:
         plt.show()
