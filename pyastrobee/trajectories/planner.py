813070588b89bcc85962a830d1820477cdaf15ec
Daniel Morton <danielpmorton@gmail.com>
2023-08-15 11:49:22 -0700

    WIP global/local planning methods for improved timing

--- pyastrobee/trajectories/planner.py
+++ pyastrobee/trajectories/planner.py
@@ -18,6 +18,16 @@ from pyastrobee.trajectories.quaternion_interpolation import (
     quaternion_interpolation_with_bcs,
 )
 from pyastrobee.utils.quaternions import quats_to_angular_velocities
+from pyastrobee.trajectories.timing import (
+    spline_duration_heuristic,
+    rotation_duration_heuristic,
+)
+from pyastrobee.config.astrobee_motion import LINEAR_SPEED_LIMIT, LINEAR_ACCEL_LIMIT
+from pyastrobee.config.iss_safe_boxes import ALL_BOXES
+from pyastrobee.config.iss_paths import GRAPH, PATHS
+from pyastrobee.utils.boxes import Box, find_containing_box_name, compute_graph
+from pyastrobee.utils.algos import dfs
+from pyastrobee.trajectories.splines import spline_trajectory_with_retiming
 
 
 def plan_trajectory(
@@ -78,3 +88,89 @@ def plan_trajectory(
         ang_accels=alpha,
         times=times,
     )
+
+
+# WORK IN PROGRESS
+# TODO handle the timing better...
+def global_planner(
+    p0: npt.ArrayLike,
+    q0: npt.ArrayLike,
+    pf: npt.ArrayLike,
+    qf: npt.ArrayLike,
+    dt: float,
+    all_boxes: dict[str, Box] = ALL_BOXES,
+    graph: Optional[dict[str, list[str]]] = GRAPH,
+):
+    # Dynamics parameters: Assume start and end from rest, satisfy operating limits
+    t0 = 0
+    v0 = np.zeros(3)
+    vf = np.zeros(3)
+    a0 = np.zeros(3)
+    af = np.zeros(3)
+    w0 = np.zeros(3)
+    wf = np.zeros(3)
+    dw0 = np.zeros(3)
+    dwf = np.zeros(3)
+    v_max = LINEAR_SPEED_LIMIT
+    a_max = LINEAR_ACCEL_LIMIT
+    # TODO add some way to know that we enforce angular speed/accel limits
+    # Retiming parameters (These are currently the same as the default)
+    kappa_min = 0.01
+    omega = 3
+    max_retiming_iters = 10
+    # Curve parameters
+    pts_per_curve = 7
+
+    # Determine the path through the safe-space graph
+    start = find_containing_box_name(p0, all_boxes)
+    end = find_containing_box_name(pf, all_boxes)
+    if graph is None:
+        graph = compute_graph(all_boxes)
+    path = dfs(graph, start, end)
+    box_path = [all_boxes[p] for p in path]
+
+    if len(box_path) == 1:
+        # Construct from a single bezier curve
+        # Can probably use the local planner here
+        # Use iterative method for retiming?? or make another function
+        # like bezier_trajector_with_retiming
+        pass
+    init_angular_duration = rotation_duration_heuristic(q0, qf)
+    init_pos_duration, init_curve_durations = spline_duration_heuristic(
+        p0, pf, box_path
+    )
+
+    duration = max(init_pos_duration, init_angular_duration)
+    # times_ = np.arange(0, duration + dt, dt)  # DO WE NEED THIS?
+    # n_timesteps = len(times_)
+    n_timesteps = int(np.ceil((duration + dt) / dt))
+
+    pos, vel, accel, times = spline_trajectory_with_retiming(
+        p0,
+        pf,
+        t0,
+        duration,
+        n_timesteps,
+        pts_per_curve,
+        box_path,
+        init_curve_durations,
+        v0,
+        vf,
+        a0,
+        af,
+        v_max,
+        a_max,
+        kappa_min,
+        omega,
+        max_retiming_iters,
+    )
+    quats = quaternion_interpolation_with_bcs(
+        q0, qf, w0, wf, dw0, dwf, duration, n_timesteps
+    )
+    omega = quats_to_angular_velocities(quats, dt)
+    alpha = np.gradient(omega, dt, axis=0)
+    return Trajectory(pos, quats, vel, omega, accel, alpha, times)
+
+
+def local_planner():
+    pass
