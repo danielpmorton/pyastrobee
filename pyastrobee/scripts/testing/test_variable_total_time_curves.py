ad02a7ee96de444b9d842c785ca8425c768ccc43
Daniel Morton <danielpmorton@gmail.com>
2023-08-18 17:34:59 -0700

    Remove variable bezier attempts that didn't work, some cleanup

--- pyastrobee/scripts/testing/test_variable_total_time_curves.py
+++ pyastrobee/scripts/testing/test_variable_total_time_curves.py
@@ -3,17 +3,22 @@
 WORK IN PROGRESS
 """
 
+from typing import Optional, Union
 
-from typing import Optional
 import cvxpy as cp
 import numpy as np
 import numpy.typing as npt
-from pyastrobee.trajectories.trajectory import Trajectory
+import pybullet
+
+from pyastrobee.trajectories.trajectory import Trajectory, plot_traj_constraints
 from pyastrobee.trajectories.bezier import BezierCurve
+from pyastrobee.trajectories.splines import CompositeBezierCurve
 from pyastrobee.utils.boxes import Box
+from pyastrobee.utils.debug_visualizer import animate_path
 from pyastrobee.config.astrobee_motion import LINEAR_SPEED_LIMIT, LINEAR_ACCEL_LIMIT
 
 
+# TODO can we separate out the binary search mechanic so that we can use this for the spline as well?
 def bezier_with_retiming(
     p0: npt.ArrayLike,
     pf: npt.ArrayLike,
@@ -27,10 +32,10 @@ def bezier_with_retiming(
     box: Optional[Box] = None,
     v_max: Optional[float] = None,
     a_max: Optional[float] = None,
-    time_weight: float = 1,  # FIGURE THIS OUT:
+    time_weight: float = 0.1,  # FIGURE THIS OUT
+    reduction_tol: float = 1e-2,
+    max_iters: int = 15,
 ):
-    max_iters = 10
-
     curve_kwargs = dict(
         p0=p0,
         pf=pf,
@@ -51,16 +56,18 @@ def bezier_with_retiming(
     best_tf = None
     best_cost = np.inf
     best_curve = None
+    prev_tf = np.inf
     cur_tf = tf
     for i in range(max_iters):
         # Solve for a curve (if possible) with the current final time
+        prev_tf = cur_tf
         curve_kwargs["tf"] = cur_tf
         try:
             curve, cost = fix_time_optimize_points(**curve_kwargs)
-        except cp.error.SolverError as e:
+        except cp.error.SolverError:
             curve, cost = None, np.inf
         # Binary search on the lowest final time based on the cost of the curve
-        print("Cost: ", cost, " for time: ", cur_tf)
+        print("Cost: ", cost, " for time: ", cur_tf, end="")
         if cost < best_cost:
             best_cost = cost
             best_curve = curve
@@ -80,9 +87,18 @@ def bezier_with_retiming(
                 cur_tf = (best_tf + cur_tf) / 2
             else:
                 cur_tf *= 2
+        reduction = abs(prev_tf - cur_tf) / cur_tf
+        print(". Percent time change: ", reduction)
+        if reduction < reduction_tol:
+            break
+    if best_curve is None:
+        # TODO use a different type of exception
+        raise ValueError("Unable to find a solution")
     return best_curve
 
 
+# TODO this is basically the same we the main bezier trajectory function
+# So, merge these
 def fix_time_optimize_points(
     p0: npt.ArrayLike,
     pf: npt.ArrayLike,
@@ -152,83 +168,23 @@ def fix_time_optimize_points(
     return solved_pos_curve, prob.value
 
 
-def _get_points(curve):
-    if isinstance(curve.points, (cp.Variable, cp.Expression)):
-        return curve.points.value
-    return curve.points
+# TODO move this to a better location
+# NOTE: Don't make a circular import since this imports both Bezier and CompositeBezier
+def traj_from_curve(
+    curve: Union[BezierCurve, CompositeBezierCurve], dt: float
+) -> Trajectory:
+    """Construct a position-only trajectory from a Bezier curve or spline
 
+    Args:
+        curve (Union[BezierCurve, CompositeBezierCurve]): Curve for the position motion
+        dt (float): Timestep
 
-def fix_points_optimize_time(
-    init_curve: BezierCurve,
-    v0: Optional[npt.ArrayLike] = None,
-    vf: Optional[npt.ArrayLike] = None,
-    a0: Optional[npt.ArrayLike] = None,
-    af: Optional[npt.ArrayLike] = None,
-    v_max: Optional[float] = None,
-    a_max: Optional[float] = None,
-    time_weight: float = 1,  # FIGURE THIS OUT
-) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    t0 = init_curve.a
-    tf = cp.Variable(1)
-    jerk_curve = BezierCurve(
-        _get_points(init_curve.derivative.derivative.derivative), t0, tf
-    )
-    # Form the constraint list depending on what was specified in the inputs
-    # NOTE: since the initial position points are fixed, we don't need to constrain p0, pf
-    constraints = [tf >= t0]
-    # Objective function criteria
-    jerk = jerk_curve.l2_squared
-    # Form the objective function based on the relative weighting between the criteria
-    objective = cp.Minimize(jerk + time_weight * (tf - t0))
-    # Form the problem and solve it
-    # Note: Clarabel is apparently better for quadratic objectives (like our jerk criteria)
-    prob = cp.Problem(objective, constraints)
-    prob.solve(solver=cp.CLARABEL)
-    if prob.status != cp.OPTIMAL:
-        raise cp.error.SolverError(
-            f"Unable to generate the trajectory (solver status: {prob.status}).\n"
-            + "Check on the feasibility of the constraints"
-        )
-    # Construct the Bezier curves from the solved control points, and return their evaluations at each time
-    solved_pos_curve = BezierCurve(_get_points(init_curve), t0, tf.value)
-    return solved_pos_curve, prob.value
-
-
-def optimal_bezier(
-    p0: npt.ArrayLike,
-    pf: npt.ArrayLike,
-    t0: float,
-    tf_init: float,
-    n_control_pts: int,
-    v0: Optional[npt.ArrayLike] = None,
-    vf: Optional[npt.ArrayLike] = None,
-    a0: Optional[npt.ArrayLike] = None,
-    af: Optional[npt.ArrayLike] = None,
-    box: Optional[Box] = None,
-    v_max: Optional[float] = None,
-    a_max: Optional[float] = None,
-    time_weight: float = 1,  # FIGURE THIS OUT
-) -> BezierCurve:
-    tf = tf_init
-    max_iters = 10
-    for i in range(max_iters):
-        # fmt: off
-        try:
-            curve_1, cost_1 = fix_time_optimize_points(p0, pf, t0, tf, n_control_pts, v0, vf, a0, af, box, v_max, a_max, time_weight)
-        except cp.error.SolverError as e:
-            # IMPROVE THIS
-            curve_1, cost_1 = fix_time_optimize_points(p0, pf, t0, tf * 5, n_control_pts, v0, vf, a0, af, box, v_max, a_max, time_weight)
-        curve_2, cost_2 = fix_points_optimize_time(curve_1, v0, vf, a0, af, v_max, a_max, time_weight)
-        tf = curve_2.b.value if isinstance(curve_2.b, (cp.Variable, cp.Expression)) else curve_2.b
-        print(f"ITERATION {i}:\nCost from point optimization: {cost_1}. Time: {curve_1.b}\n" + 
-              f"Cost from time optimization: {cost_2}. Time: {tf}")
-    return curve_2
-    # fmt: on
-
-
-def traj_from_bezier(curve: BezierCurve, dt: float) -> Trajectory:
+    Returns:
+        Trajectory: Position (and derivatives) trajectory information
+    """
     t0 = curve.a
     tf = curve.b.value if isinstance(curve.b, (cp.Variable, cp.Expression)) else curve.b
+    # TODO see if we can refine how this time works... The spacing isn't going to be exactly dt
     times = np.linspace(t0, tf, round((tf - t0) / dt))
     pos = curve(times)
     vel = curve.derivative(times)
@@ -241,13 +197,13 @@ def main():
     pf = (1, 2, 3)
     t0 = 0
     tf_init = 20
-    n_control_pts = 8
+    n_control_pts = 30
     dt = 0.1
-    v0 = (0.1, 0.2, 0.3)
+    v0 = (0.3, 0.2, 0.1)
     vf = (0, 0, 0)
     a0 = (0, 0, 0)
     af = (0, 0, 0)
-    time_weight = 1
+    time_weight = 0.1
     print("Speed limit: ", LINEAR_SPEED_LIMIT)
     print("Accel limit: ", LINEAR_ACCEL_LIMIT)
     # curve = optimal_bezier(
@@ -266,8 +222,15 @@ def main():
         LINEAR_ACCEL_LIMIT,
         time_weight,
     )
-    traj = traj_from_bezier(curve, dt)
+    traj = traj_from_curve(curve, dt)
     traj.plot()
+    plot_traj_constraints(
+        traj, None, LINEAR_SPEED_LIMIT, LINEAR_ACCEL_LIMIT, None, None
+    )
+    pybullet.connect(pybullet.GUI)
+    traj.visualize(30)
+    animate_path(traj.positions, traj.num_timesteps // 5)
+    input("Animation complete, press Enter to finish")
 
 
 if __name__ == "__main__":
