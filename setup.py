bc24d14b313444de12fc5fed12e966898a5c733b
Daniel Morton <danielpmorton@gmail.com>
2025-04-18 11:11:43 -0700

    Update dependencies...
    Remove unused packages that I had been using for testing
    Move pylint/black/portray into a dev dependency section

--- setup.py
+++ setup.py
@@ -7,13 +7,6 @@ including some custom changes, which may not be available on pypi for some time.
 of pybullet from source instead.
 """
 
-# TODO add specific versions to the package requirements
-# TODO add obj2mjcf to the dev requirements?
-# TODO add beautifulsoup4 back in if parsing XML/URDFs
-# TODO add open3d back in if visualizing anything 3D outside of pybullet
-# TODO add mujoco back if needed
-# TODO add slycot back in if the control package needs it. Seems to have installation problems
-
 from setuptools import setup, find_packages
 
 setup(
@@ -21,25 +14,25 @@ setup(
     version="0.0.1",
     install_requires=[
         # "pybullet",  # Simulation. See notes about locally-built version
-        "numpy==1.26.4",  # Needs to be installed before Pybullet to enable speedup for matrix ops
+        "numpy<2",  # Needs to be installed before Pybullet
         "wheel",  # For helping build Pybullet
-        "opencv-python",  # Vision
         "matplotlib",  # Plotting
         "pytransform3d",  # Manages rotations and transformations
         "pynput",  # For keyboard event listeners
-        "ahrs",  # For some quaternion math and test cases
-        "pylint",  # Linting
-        "black",  # Formatting
-        "ipython",  # Interactive sessions
-        "control",  # Control systems reference code
         "cvxpy",  # Optimization
         "clarabel",  # More optimization
         "mosek",  # More optimization
         "stable-baselines3",  # Environments
         "gymnasium",  # Environments
-        "portray",  # Documentation
     ],
-    description="Code for the IPRL Astrobee project",
+    extras_require={
+        "dev": [
+            "pylint",  # Linting
+            "black",  # Formatting
+            "portray",  # Documentation
+        ],
+    },
+    description="A simulation environment for Astrobee in Python",
     author="Daniel Morton",
     author_email="danielpmorton@gmail.com",
     url="https://github.com/danielpmorton/pyastrobee",
